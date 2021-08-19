52fc253198af1e09cd1400c56453a6e8797f7277
aknysh <andriy.knysh@gmail.com>
2021-08-19 16:18:53 -0400

    Updates

--- cmd/terraform.go
+++ cmd/terraform.go
@@ -9,33 +9,48 @@ import (
 
 // terraformCmd represents the base command for all terraform sub-commands
 var terraformCmd = &cobra.Command{
-	Use:   "terraform",
-	Short: "Terraform command",
-	Long:  `This command runs terraform sub-commands`,
-	// FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: true},
+	Use:                "terraform",
+	Short:              "Terraform command",
+	Long:               `This command runs terraform sub-commands`,
+	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: true},
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print("Args: ")
+		fmt.Println(args)
+		fmt.Println()
+
+		cmd.DisableFlagParsing = false
+		err := cmd.ParseFlags(args)
+		if err != nil {
+			return
+		}
 		flags := cmd.Flags()
 
 		fmt.Println("Flags: ")
-
 		flags.Visit(func(flag *pflag.Flag) {
 			fmt.Println(flag.Name + ": " + flag.Value.String())
 		})
+
+		_, args2, err := cmd.Traverse(args)
+		if err != nil {
+			return
+		}
+
+		fmt.Print("Args2: ")
+		fmt.Println(args2)
+		fmt.Println()
 
 		stack, err := flags.GetString("stack")
 		if err != nil {
 			log.Fatalln(err)
 			return
 		}
-
-		fmt.Println(args)
 		fmt.Println("Stack: " + stack)
 	},
 }
 
 func init() {
 	// https://github.com/spf13/cobra/issues/739
-	terraformCmd.DisableFlagParsing = false
+	terraformCmd.DisableFlagParsing = true
 	terraformCmd.PersistentFlags().StringP("stack", "s", "", "")
 	terraformCmd.PersistentFlags().StringP("command", "c", "", "")
 
---
+++ internal/exec/terraform.go
@@ -0,0 +1,6 @@
+package exec
+
+// ExecuteTerraform executes terraform commands
+func ExecuteTerraform() {
+
+}
