997cc781942b7fe28a85b5c880bcfe0afe0a73a4
aknysh <andriy.knysh@gmail.com>
2021-08-24 00:02:05 -0400

    Updates

--- internal/exec/terraform.go
+++ internal/exec/terraform.go
@@ -2,7 +2,9 @@ package exec
 
 import (
 	c "atmos/internal/config"
+	s "atmos/internal/stack"
 	u "atmos/internal/utils"
+	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -13,7 +15,7 @@ import (
 // ExecuteTerraform executes terraform commands
 func ExecuteTerraform(cmd *cobra.Command, args []string) error {
 	if len(args) < 3 {
-		return errors.New("invalid number of arguments")
+		return errors.New("Invalid number of arguments")
 	}
 
 	cmd.DisableFlagParsing = false
@@ -24,20 +26,24 @@ func ExecuteTerraform(cmd *cobra.Command, args []string) error {
 	}
 	flags := cmd.Flags()
 
+	// Get stack
 	stack, err := flags.GetString("stack")
 	if err != nil {
 		return err
 	}
 
+	// Process and merge CLI configurations
 	err = c.InitConfig(stack)
 	if err != nil {
 		return err
 	}
 
+	// Process CLI arguments and flags
 	additionalArgsAndFlags := removeCommonArgsAndFlags(args)
 	subCommand := args[0]
 	allArgsAndFlags := append([]string{subCommand}, additionalArgsAndFlags...)
 
+	// Find and check component
 	component := args[1]
 	if len(component) < 1 {
 		return errors.New("'component' is required")
@@ -55,6 +61,19 @@ func ExecuteTerraform(cmd *cobra.Command, args []string) error {
 	fmt.Printf("Additional arguments: %v\n", additionalArgsAndFlags)
 	fmt.Println(strings.Repeat("-", 120))
 
+	// Process stack config file(s)
+	_, stacksMap, err := s.ProcessYAMLConfigFiles(c.Config.StackConfigFiles, false, true)
+	if err != nil {
+		return err
+	}
+
+	j, _ := json.MarshalIndent(stacksMap, "", strings.Repeat(" ", 2))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", j)
+
+	// Execute command
 	command := "terraform"
 	fmt.Println(fmt.Sprintf("Executing command: %s %s %s", command,
 		subCommand, u.SliceOfStringsToSpaceSeparatedString(additionalArgsAndFlags)))
