f2d857ab47e9326b352854f550bf05c1bfcdbe2d
aknysh <andriy.knysh@gmail.com>
2021-12-20 21:24:23 -0500

    `atmos help`

--- internal/exec/help.go
+++ internal/exec/help.go
@@ -1,10 +1,34 @@
 package exec
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
 
 // processHelp processes help commands
 func processHelp(componentType string, command string) error {
-	message := fmt.Sprintf("Help for atmos %s %s", componentType, command)
-	fmt.Println(message)
+	if len(command) == 0 {
+		fmt.Println(fmt.Sprintf("'atmos' supports all native '%s' commands.", componentType))
+		fmt.Println(fmt.Sprintf("In addition, 'component' and 'stack' are required in order to generate variables for the component in the stack."))
+		color.Cyan(fmt.Sprintf("atmos %s <command> <component> -s <stack> [options]", componentType))
+		color.Cyan(fmt.Sprintf("atmos %s <command> <component> --stack <stack> [options]", componentType))
+
+		err := execCommand(componentType, []string{"--help"}, "", nil)
+		if err != nil {
+			return err
+		}
+	} else {
+		fmt.Println(fmt.Sprintf("'atmos' supports native '%s %s' command with all the options, arguments and flags.", componentType, command))
+		fmt.Println(fmt.Sprintf("In addition, 'component' and 'stack' are required in order to generate variables for the component in the stack."))
+		color.Cyan(fmt.Sprintf("atmos %s %s <component> -s <stack> [options]", componentType, command))
+		color.Cyan(fmt.Sprintf("atmos %s %s <component> --stack <stack> [options]", componentType, command))
+
+		err := execCommand(componentType, []string{command, "--help"}, "", nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println()
 	return nil
 }
