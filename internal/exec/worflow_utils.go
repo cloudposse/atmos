dfbbb861a6fc38e6b0243926dd43025fdc871475
aknysh <andriy.knysh@gmail.com>
2022-03-09 20:25:02 -0500

    Add workflows

--- internal/exec/worflow_utils.go
+++ internal/exec/worflow_utils.go
@@ -12,14 +12,14 @@ func executeWorkflowSteps(workflowDefinition c.WorkflowDefinition) error {
 	var steps = workflowDefinition.Steps
 
 	for _, step := range steps {
-		var command = step.Command
-		var commandType = step.Type
+		var command = strings.TrimSpace(step.Command)
+		var commandType = strings.TrimSpace(step.Type)
 
 		if commandType == "" {
 			commandType = "atmos"
 		}
 
-		color.Cyan(fmt.Sprintf("Executing workflow step: %s", command))
+		color.HiCyan(fmt.Sprintf("Executing workflow step: %s", command))
 
 		if commandType == "shell" {
 			args := strings.Fields(command)
@@ -28,11 +28,28 @@ func executeWorkflowSteps(workflowDefinition c.WorkflowDefinition) error {
 			}
 		} else if commandType == "atmos" {
 			args := strings.Fields(command)
+
+			var workflowStack = strings.TrimSpace(workflowDefinition.Stack)
+			var stepStack = strings.TrimSpace(step.Stack)
+			var finalStack = ""
+
+			if stepStack != "" {
+				args = append(args, []string{"-s", stepStack}...)
+				finalStack = stepStack
+			} else if workflowStack != "" {
+				args = append(args, []string{"-s", workflowStack}...)
+				finalStack = workflowStack
+			}
+
+			if finalStack != "" {
+				color.HiCyan(fmt.Sprintf("Stack: %s", finalStack))
+			}
+
 			if err := execCommand("atmos", args, ".", []string{}); err != nil {
 				return err
 			}
 		} else {
-			return errors.New(fmt.Sprintf("invalid workflow step type '%s'", commandType))
+			return errors.New(fmt.Sprintf("invalid workflow step type '%s'. Supported types are 'atmos' and 'shell'", commandType))
 		}
 
 		fmt.Println()
