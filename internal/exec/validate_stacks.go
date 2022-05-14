9dcfdb5898815efb81ae51791bc1b8fcb1e4ab89
aknysh <andriy.knysh@gmail.com>
2022-05-14 10:06:23 -0400

    update `validate stacks`

--- internal/exec/validate_stacks.go
+++ internal/exec/validate_stacks.go
@@ -1,11 +1,13 @@
 package exec
 
 import (
+	"fmt"
 	c "github.com/cloudposse/atmos/pkg/config"
 	s "github.com/cloudposse/atmos/pkg/stack"
 	u "github.com/cloudposse/atmos/pkg/utils"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"path"
 	"strings"
 )
 
@@ -22,23 +24,24 @@ func ExecuteValidateStacks(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	// Include (process and validate) all YAML files in the `stacks` folder in all subfolders
 	includedPaths := []string{"**/*"}
+	// Don't exclude any YAML files for validation
+	excludedPaths := []string{}
 	includeStackAbsPaths, err := u.JoinAbsolutePathWithPaths(c.ProcessedConfig.StacksBaseAbsolutePath, includedPaths)
 	if err != nil {
 		return err
 	}
 
-	stackConfigFilesAbsolutePaths, _, err := c.FindAllStackConfigsInPaths(
-		includeStackAbsPaths,
-		[]string{},
-	)
-
+	stackConfigFilesAbsolutePaths, _, err := c.FindAllStackConfigsInPaths(includeStackAbsPaths, excludedPaths)
 	if err != nil {
 		return err
 	}
 
-	var errorMessages []string
+	u.PrintInfo(fmt.Sprintf("Validating all YAML files in the '%s' folder and all subfolders\n",
+		path.Join(c.Config.BasePath, c.Config.Stacks.BasePath)))
 
+	var errorMessages []string
 	for _, filePath := range stackConfigFilesAbsolutePaths {
 		_, _, err = s.ProcessYAMLConfigFile(c.ProcessedConfig.StacksBaseAbsolutePath, filePath, map[string]map[interface{}]interface{}{})
 		if err != nil {
