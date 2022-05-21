20b4a612b2af0b628046bcb9676dc96ba19f7910
Andriy Knysh <aknysh@users.noreply.github.com>
2022-05-20 23:12:59 -0400

    Improve `atmos vendor pull` command. Add more vendoring examples and tests (#151)
    * Improve `atmos vendor pull` command. Add more vendoring examples and tests
    
    * Improve `atmos vendor pull` command. Add more vendoring examples and tests

--- pkg/vender/component_vendor_test.go
+++ pkg/vender/component_vendor_test.go
@@ -10,13 +10,14 @@ import (
 )
 
 func TestVendorComponentPullCommand(t *testing.T) {
-	component := "infra/vpc-flow-logs-bucket"
-	componentType := "terraform"
-	vendorCommand := "pull"
-
 	err := c.InitConfig()
 	assert.Nil(t, err)
 
+	componentType := "terraform"
+	vendorCommand := "pull"
+
+	// Test 'infra/vpc-flow-logs-bucket' component
+	component := "infra/vpc-flow-logs-bucket"
 	componentConfig, componentPath, err := e.ReadAndProcessComponentConfigFile(component, componentType)
 	assert.Nil(t, err)
 
@@ -52,5 +53,77 @@ func TestVendorComponentPullCommand(t *testing.T) {
 	err = os.Remove(path.Join(componentPath, "variables.tf"))
 	assert.Nil(t, err)
 	err = os.Remove(path.Join(componentPath, "versions.tf"))
+	assert.Nil(t, err)
+
+	// Test 'infra/account-map' component
+	component = "infra/account-map"
+	componentConfig, componentPath, err = e.ReadAndProcessComponentConfigFile(component, componentType)
+	assert.Nil(t, err)
+
+	err = e.ExecuteComponentVendorCommandInternal(componentConfig.Spec, component, componentPath, false, vendorCommand)
+	assert.Nil(t, err)
+
+	// Check if the correct files were pulled and written to the correct folder
+	assert.FileExists(t, path.Join(componentPath, "context.tf"))
+	assert.FileExists(t, path.Join(componentPath, "default.auto.tfvars"))
+	assert.FileExists(t, path.Join(componentPath, "introspection.mixin.tf"))
+	assert.FileExists(t, path.Join(componentPath, "main.tf"))
+	assert.FileExists(t, path.Join(componentPath, "outputs.tf"))
+	assert.FileExists(t, path.Join(componentPath, "providers.tf"))
+	assert.FileExists(t, path.Join(componentPath, "README.md"))
+	assert.FileExists(t, path.Join(componentPath, "remote-state.tf"))
+	assert.FileExists(t, path.Join(componentPath, "variables.tf"))
+	assert.FileExists(t, path.Join(componentPath, "versions.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "iam-roles", "context.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "iam-roles", "main.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "iam-roles", "outputs.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "iam-roles", "variables.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "roles-to-principals", "context.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "roles-to-principals", "main.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "roles-to-principals", "outputs.tf"))
+	assert.FileExists(t, path.Join(componentPath, "modules", "roles-to-principals", "variables.tf"))
+
+	// Delete the files and folders
+	err = os.Remove(path.Join(componentPath, "context.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "default.auto.tfvars"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "introspection.mixin.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "main.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "outputs.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "providers.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "README.md"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "remote-state.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "variables.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "versions.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "iam-roles", "context.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "iam-roles", "main.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "iam-roles", "outputs.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "iam-roles", "variables.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "roles-to-principals", "context.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "roles-to-principals", "main.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "roles-to-principals", "outputs.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "roles-to-principals", "variables.tf"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "iam-roles"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules", "roles-to-principals"))
+	assert.Nil(t, err)
+	err = os.Remove(path.Join(componentPath, "modules"))
 	assert.Nil(t, err)
 }
