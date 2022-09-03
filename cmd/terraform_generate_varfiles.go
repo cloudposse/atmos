9041ff2e83d7cf921ca28d0d8fbf777675b2822d
aknysh <andriy.knysh@gmail.com>
2022-09-03 19:02:23 -0400

    Add `terraform generate varfiles` command

--- cmd/terraform_generate_varfiles.go
+++ cmd/terraform_generate_varfiles.go
@@ -10,7 +10,7 @@ import (
 var terraformGenerateVarfilesCmd = &cobra.Command{
 	Use:                "varfiles",
 	Short:              "Execute 'terraform generate varfiles' command",
-	Long:               `This command generates varfiles for all component in each stack`,
+	Long:               `This command generates varfiles for all terraform components in all stacks`,
 	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: false},
 	Run: func(cmd *cobra.Command, args []string) {
 		err := e.ExecuteTerraformGenerateVarfiles(cmd, args)
@@ -22,8 +22,10 @@ var terraformGenerateVarfilesCmd = &cobra.Command{
 
 func init() {
 	terraformGenerateVarfilesCmd.DisableFlagParsing = false
-	terraformGenerateVarfilesCmd.PersistentFlags().StringP("stack", "s", "", "atmos terraform generate varfile <component> -s <stack>")
-	terraformGenerateVarfilesCmd.PersistentFlags().StringP("file", "f", "", "atmos terraform generate varfile <component> -s <stack> -f <file>")
+	terraformGenerateVarfilesCmd.PersistentFlags().StringP("stack", "s", "", "atmos terraform generate varfiles <component> -s <stack>")
+	terraformGenerateVarfilesCmd.PersistentFlags().String("file", "", "atmos terraform generate varfiles --file <file_template>")
+	terraformGenerateVarfilesCmd.PersistentFlags().String("format", "yaml", "Specify output format: atmos terraform generate varfiles --format=yaml/json ('json' is default)")
+	terraformGenerateVarfilesCmd.PersistentFlags().String("components", "", "Filter by specific components: atmos terraform generate varfiles --components=<component1>,<component2>")
 
 	err := terraformGenerateVarfilesCmd.MarkPersistentFlagRequired("stack")
 	if err != nil {
--- internal/exec/terraform_generate_varfiles.go
+++ internal/exec/terraform_generate_varfiles.go
@@ -1,70 +1,155 @@
 package exec
 
 import (
-	"errors"
 	"fmt"
 	c "github.com/cloudposse/atmos/pkg/config"
+	s "github.com/cloudposse/atmos/pkg/stack"
 	u "github.com/cloudposse/atmos/pkg/utils"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
-// ExecuteTerraformGenerateVarfile executes `terraform generate varfile` command
+// ExecuteTerraformGenerateVarfiles executes `terraform generate varfiles` command
 func ExecuteTerraformGenerateVarfiles(cmd *cobra.Command, args []string) error {
-	if len(args) != 1 {
-		return errors.New("invalid arguments. The command requires one argument `component`")
-	}
-
 	flags := cmd.Flags()
 
-	stack, err := flags.GetString("stack")
+	filterByStack, err := flags.GetString("stack")
 	if err != nil {
 		return err
 	}
 
-	component := args[0]
+	format, err := flags.GetString("format")
+	if err != nil {
+		return err
+	}
+	if format != "" && format != "yaml" && format != "json" {
+		return fmt.Errorf("invalid '--format' flag '%s'. Valid values are 'yaml' (default) and 'json'", format)
+	}
+	if format == "" {
+		format = "yaml"
+	}
 
-	var info c.ConfigAndStacksInfo
-	info.ComponentFromArg = component
-	info.Stack = stack
-	info.ComponentType = "terraform"
-
-	info, err = ProcessStacks(info, true)
+	file, err := flags.GetString("file")
 	if err != nil {
 		return err
 	}
 
-	var varFileNameFromArg string
-	var varFilePath string
-
-	varFileNameFromArg, err = flags.GetString("file")
+	componentsCsv, err := flags.GetString("components")
 	if err != nil {
-		varFileNameFromArg = ""
+		return err
+	}
+	var components []string
+	if componentsCsv != "" {
+		components = strings.Split(componentsCsv, ",")
 	}
 
-	if len(varFileNameFromArg) > 0 {
-		varFilePath = varFileNameFromArg
-	} else {
-		varFilePath = constructTerraformComponentVarfilePath(info)
+	componentTypesCsv, err := flags.GetString("component-types")
+	if err != nil {
+		return err
+	}
+	var componentTypes []string
+	if componentTypesCsv != "" {
+		componentTypes = strings.Split(componentTypesCsv, ",")
 	}
 
-	// Print the component variables
-	u.PrintInfo(fmt.Sprintf("\nVariables for the component '%s' in the stack '%s':\n", info.ComponentFromArg, info.Stack))
-	err = u.PrintAsYAML(info.ComponentVarsSection)
+	sectionsCsv, err := flags.GetString("sections")
+	if err != nil {
+		return err
+	}
+	var sections []string
+	if sectionsCsv != "" {
+		sections = strings.Split(sectionsCsv, ",")
+	}
+
+	var configAndStacksInfo c.ConfigAndStacksInfo
+	configAndStacksInfo.Stack = filterByStack
+	stacksMap, err := FindStacksMap(configAndStacksInfo, filterByStack != "")
 	if err != nil {
 		return err
 	}
 
-	// Write the variables to file
-	u.PrintInfo("Writing the variables to file:")
-	fmt.Println(varFilePath)
+	finalStacksMap := make(map[string]any)
 
-	if !info.DryRun {
-		err = u.WriteToFileAsJSON(varFilePath, info.ComponentVarsSection, 0644)
-		if err != nil {
-			return err
+	for stackName, stackSection := range stacksMap {
+		if filterByStack == "" || filterByStack == stackName {
+			// Delete the stack-wide imports
+			delete(stackSection.(map[any]any), "imports")
+
+			if !u.MapKeyExists(finalStacksMap, stackName) {
+				finalStacksMap[stackName] = make(map[string]any)
+			}
+
+			if componentsSection, ok := stackSection.(map[any]any)["components"].(map[string]any); ok {
+				if len(componentTypes) == 0 || u.SliceContainsString(componentTypes, "terraform") {
+					if terraformSection, ok := componentsSection["terraform"].(map[string]any); ok {
+						for componentName, compSection := range terraformSection {
+							componentSection, ok := compSection.(map[string]any)
+							if !ok {
+								return fmt.Errorf("invalid 'components.terraform.%s' section in the file '%s'", componentName, stackName)
+							}
+
+							// Find all derived components of the provided components and include them in the output
+							derivedComponents, err := s.FindComponentsDerivedFromBaseComponents(stackName, terraformSection, components)
+							if err != nil {
+								return err
+							}
+
+							if len(components) == 0 || u.SliceContainsString(components, componentName) || u.SliceContainsString(derivedComponents, componentName) {
+								if !u.MapKeyExists(finalStacksMap[stackName].(map[string]any), "components") {
+									finalStacksMap[stackName].(map[string]any)["components"] = make(map[string]any)
+								}
+								if !u.MapKeyExists(finalStacksMap[stackName].(map[string]any)["components"].(map[string]any), "terraform") {
+									finalStacksMap[stackName].(map[string]any)["components"].(map[string]any)["terraform"] = make(map[string]any)
+								}
+								if !u.MapKeyExists(finalStacksMap[stackName].(map[string]any)["components"].(map[string]any)["terraform"].(map[string]any), componentName) {
+									finalStacksMap[stackName].(map[string]any)["components"].(map[string]any)["terraform"].(map[string]any)[componentName] = make(map[string]any)
+								}
+
+								for sectionName, section := range componentSection {
+									if len(sections) == 0 || u.SliceContainsString(sections, sectionName) {
+										finalStacksMap[stackName].(map[string]any)["components"].(map[string]any)["terraform"].(map[string]any)[componentName].(map[string]any)[sectionName] = section
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			// Filter out empty stacks (stacks without any components)
+			if st, ok := finalStacksMap[stackName].(map[string]any); ok {
+				if len(st) == 0 {
+					delete(finalStacksMap, stackName)
+				}
+			}
 		}
 	}
 
-	fmt.Println()
+	if format == "yaml" {
+		if file == "" {
+			err = u.PrintAsYAML(finalStacksMap)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = u.WriteToFileAsYAML(file, finalStacksMap, 0644)
+			if err != nil {
+				return err
+			}
+		}
+	} else if format == "json" {
+		if file == "" {
+			err = u.PrintAsJSON(finalStacksMap)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = u.WriteToFileAsJSON(file, finalStacksMap, 0644)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
