4b129b583865adec1e009ca1158875ec6e22b95b
aknysh <andriy.knysh@gmail.com>
2022-09-04 12:34:04 -0400

    Add `terraform generate varfiles` command

--- cmd/terraform_generate_varfiles.go
+++ cmd/terraform_generate_varfiles.go
@@ -22,11 +22,28 @@ var terraformGenerateVarfilesCmd = &cobra.Command{
 
 func init() {
 	terraformGenerateVarfilesCmd.DisableFlagParsing = false
-	terraformGenerateVarfilesCmd.PersistentFlags().String("path-template", "", "atmos terraform generate varfiles --path-template <path_template>")
-	terraformGenerateVarfilesCmd.PersistentFlags().String("stacks", "", "Filter by specific stacks: atmos terraform generate varfiles --path-template <path_template> --stacks <stack1>,<stack2>")
-	terraformGenerateVarfilesCmd.PersistentFlags().String("components", "", "Filter by specific components: atmos terraform generate varfiles --path-template <path_template> --components=<component1>,<component2>")
 
-	err := terraformGenerateVarfilesCmd.MarkPersistentFlagRequired("path-template")
+	terraformGenerateVarfilesCmd.PersistentFlags().String("file-template", "",
+		"Varfile template (the file path, file name, and file extension).\n"+
+			"The file extension from the template will be applied to all generated varfiles\n"+
+			"Supports absolute and relative paths.\n"+
+			"Supports context tokens: {namespace}, {tenant}, {environment}, {stage}, {component}, {component_path}.\n"+
+			"atmos terraform generate varfiles --file-template {component_path}/{environment}-{stage}.tfvars.json\n"+
+			"atmos terraform generate varfiles --file-template /configs/{tenant}/{environment}/{stage}/{component}.json\n"+
+			"atmos terraform generate varfiles --file-template /{tenant}/{environment}/{stage}/{component}.yaml",
+	)
+
+	terraformGenerateVarfilesCmd.PersistentFlags().String("stacks", "",
+		"Only process the specified stacks (comma-separated values).\n"+
+			"atmos terraform generate varfiles --file-template <file_template> --stacks <stack1>,<stack2>",
+	)
+
+	terraformGenerateVarfilesCmd.PersistentFlags().String("components", "",
+		"Only process the specified components (comma-separated values).\n"+
+			"atmos terraform generate varfiles --file-template <file_template> --components=<component1>,<component2>",
+	)
+
+	err := terraformGenerateVarfilesCmd.MarkPersistentFlagRequired("file-template")
 	if err != nil {
 		u.PrintErrorToStdErrorAndExit(err)
 	}
