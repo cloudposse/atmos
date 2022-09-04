fe055d85222dae880da4b7abf9a0617de3e5045e
aknysh <andriy.knysh@gmail.com>
2022-09-04 01:34:46 -0400

    Add `terraform generate varfiles` command

--- internal/exec/terraform_generate_varfiles.go
+++ internal/exec/terraform_generate_varfiles.go
@@ -1,7 +1,6 @@
 package exec
 
 import (
-	"fmt"
 	c "github.com/cloudposse/atmos/pkg/config"
 	s "github.com/cloudposse/atmos/pkg/stack"
 	u "github.com/cloudposse/atmos/pkg/utils"
@@ -44,27 +43,31 @@ func ExecuteTerraformGenerateVarfiles(stacks []string, components []string) erro
 
 	for stackName, stackSection := range stacksMap {
 		if len(stacks) == 0 || u.SliceContainsString(stacks, stackName) {
-			u.PrintInfo(fmt.Sprintf("Processing stack '%s'", stackName))
-
 			if componentsSection, ok := stackSection.(map[any]any)["components"].(map[string]any); ok {
 				if terraformSection, ok := componentsSection["terraform"].(map[string]any); ok {
 					for componentName, compSection := range terraformSection {
-						componentSection, ok := compSection.(map[string]any)
-						if !ok {
-							return fmt.Errorf("invalid 'components.terraform.%s' section in the file '%s'", componentName, stackName)
-						}
+						if componentSection, ok := compSection.(map[string]any); ok {
+							// Find all derived components of the provided components
+							derivedComponents, err := s.FindComponentsDerivedFromBaseComponents(stackName, terraformSection, components)
+							if err != nil {
+								return err
+							}
 
-						// Find all derived components of the provided components
-						derivedComponents, err := s.FindComponentsDerivedFromBaseComponents(stackName, terraformSection, components)
-						if err != nil {
-							return err
-						}
+							if len(components) == 0 || u.SliceContainsString(components, componentName) || u.SliceContainsString(derivedComponents, componentName) {
+								if varsSection, ok := componentSection["vars"].(map[any]any); ok {
+									context := c.GetContextFromVars(varsSection)
 
-						if len(components) == 0 || u.SliceContainsString(components, componentName) || u.SliceContainsString(derivedComponents, componentName) {
-							if varsSection, ok := componentSection["vars"].(map[any]any); ok {
-								u.PrintInfo(fmt.Sprintf("Processing component '%s'", componentName))
-								context := c.GetContextFromVars(varsSection)
-								u.PrintInfo(context.Component)
+									if context.Component == "" {
+
+									}
+								}
+
+								terraformComponent := componentName
+								if componentAttribute, ok := componentSection["component"].(string); ok {
+									terraformComponent = componentAttribute
+								}
+
+								u.PrintInfo(terraformComponent)
 							}
 						}
 					}
