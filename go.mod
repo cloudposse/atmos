29329d77b56b17df4c3977a2c9351f1b847cd400
aknysh <andriy.knysh@gmail.com>
2022-09-25 22:32:34 -0400

    Add schemas and validation

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/otiai10/copy v1.7.0
 	github.com/pkg/errors v0.9.1
+	github.com/santhosh-tekuri/jsonschema/v5 v5.0.1
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.13.0
 	github.com/stretchr/testify v1.8.0
--- internal/exec/validate_component.go
+++ internal/exec/validate_component.go
@@ -5,7 +5,9 @@ import (
 	c "github.com/cloudposse/atmos/pkg/config"
 	u "github.com/cloudposse/atmos/pkg/utils"
 	"github.com/pkg/errors"
+	"github.com/santhosh-tekuri/jsonschema/v5"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 // ExecuteValidateComponentCmd executes `validate component` command
@@ -32,11 +34,14 @@ func ExecuteValidateComponentCmd(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
+	if schemaType != "jsonschema" && schemaType != "opa" && schemaType != "cue" {
+		return fmt.Errorf("invalid 'schema-type=%s' argument. Supported values: jsonschema, opa, cue", schemaType)
+	}
 
 	return ExecuteValidateComponent(component, stack, schemaPath, schemaType)
 }
 
-// ExecuteValidateComponent validates a component in a stack using JsonSchema, OPA or CUE schema
+// ExecuteValidateComponent validates a component in a stack using JsonSchema, OPA or CUE schema documents
 func ExecuteValidateComponent(component string, stack string, schemaPath string, schemaType string) error {
 	var configAndStacksInfo c.ConfigAndStacksInfo
 	configAndStacksInfo.ComponentFromArg = component
@@ -59,5 +64,36 @@ func ExecuteValidateComponent(component string, stack string, schemaPath string,
 		return err
 	}
 
+	return nil
+}
+
+// validateWithJsonSchema validates the data structure using the provided JSON Schema document
+// https://github.com/santhosh-tekuri/jsonschema
+// https://go.dev/play/p/Hhax3MrtD8r
+func validateWithJsonSchema(data any, schemaText string) error {
+	compiler := jsonschema.NewCompiler()
+	if err := compiler.AddResource("schema.json", strings.NewReader(schemaText)); err != nil {
+		return err
+	}
+
+	schema, err := compiler.Compile("schema.json")
+	if err != nil {
+		return err
+	}
+
+	if err = schema.Validate(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// validateWithOpa validates the data structure using the provided OPA schema document
+func validateWithOpa(data any, schemaText string) error {
+	return nil
+}
+
+// validateWithCue validates the data structure using the provided CUE schema document
+func validateWithCue(data any, schemaText string) error {
 	return nil
 }
