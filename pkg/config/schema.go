b04a51bce915aaccdb75017dbbb8b8eaef3bc659
aknysh <andriy.knysh@gmail.com>
2022-11-02 00:11:25 -0400

    Expose component config in custom commands

--- pkg/config/schema.go
+++ pkg/config/schema.go
@@ -211,14 +211,15 @@ type VendorComponentConfig struct {
 // Custom CLI commands
 
 type Command struct {
-	Name        string            `yaml:"name" json:"name" mapstructure:"name"`
-	Description string            `yaml:"description" json:"description" mapstructure:"description"`
-	Env         []CommandEnv      `yaml:"env" json:"env" mapstructure:"env"`
-	Arguments   []CommandArgument `yaml:"arguments" json:"arguments" mapstructure:"arguments"`
-	Flags       []CommandFlag     `yaml:"flags" json:"flags" mapstructure:"flags"`
-	Steps       []string          `yaml:"steps" json:"steps" mapstructure:"steps"`
-	Commands    []Command         `yaml:"commands" json:"commands" mapstructure:"commands"`
-	Verbose     bool              `yaml:"verbose" json:"verbose" mapstructure:"verbose"`
+	Name            string                 `yaml:"name" json:"name" mapstructure:"name"`
+	Description     string                 `yaml:"description" json:"description" mapstructure:"description"`
+	Env             []CommandEnv           `yaml:"env" json:"env" mapstructure:"env"`
+	Arguments       []CommandArgument      `yaml:"arguments" json:"arguments" mapstructure:"arguments"`
+	Flags           []CommandFlag          `yaml:"flags" json:"flags" mapstructure:"flags"`
+	ComponentConfig CommandComponentConfig `yaml:"component_config" json:"component_config" mapstructure:"component_config"`
+	Steps           []string               `yaml:"steps" json:"steps" mapstructure:"steps"`
+	Commands        []Command              `yaml:"commands" json:"commands" mapstructure:"commands"`
+	Verbose         bool                   `yaml:"verbose" json:"verbose" mapstructure:"verbose"`
 }
 
 type CommandArgument struct {
@@ -239,6 +240,11 @@ type CommandEnv struct {
 	Key          string `yaml:"key" json:"key" mapstructure:"key"`
 	Value        string `yaml:"value" json:"value" mapstructure:"value"`
 	ValueCommand string `yaml:"valueCommand" json:"valueCommand" mapstructure:"valueCommand"`
+}
+
+type CommandComponentConfig struct {
+	Component string `yaml:"component" json:"component" mapstructure:"component"`
+	Stack     string `yaml:"stack" json:"stack" mapstructure:"stack"`
 }
 
 // Integrations
