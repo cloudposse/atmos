f30071cd3b90d0a8ee32b1c55fbbd3de4412fdd9
aknysh <andriy.knysh@gmail.com>
2022-11-06 11:59:22 -0500

    Update docs

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -10,17 +9,10 @@ import styles from './index.module.css';
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <header className={clsx('hero', styles.heroBanner)}>
             <div className="container">
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/intro">
-                        Atmos Docs
-                    </Link>
-                </div>
             </div>
         </header>
     );
@@ -31,7 +23,7 @@ export default function Home() {
     return (
         <Layout
             title={`${siteConfig.title}`}
-            description="`atmos` is a universal tool for DevOps and Cloud Automation">
+            description="atmos is a universal tool for DevOps and Cloud Automation">
             <HomepageHeader/>
             <main>
                 <HomepageFeatures/>
