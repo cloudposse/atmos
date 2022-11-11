6abd0a81ba7aaac2c11073b8a589da4ed306a2c3
aknysh <andriy.knysh@gmail.com>
2022-11-10 20:08:48 -0500

    Updates

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,22 +1,9 @@
 import React from "react";
-import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css"
-
-function Header() {
-    const {siteConfig} = useDocusaurusContext();
-    return (
-        <header className={clsx("hero", styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-            </div>
-        </header>
-    );
-}
 
 // https://github.com/xiaolin/react-image-gallery
 // https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
@@ -34,15 +21,14 @@ const images = Array.from({length: 27}, (_, i) => {
     }
 );
 
-export default function Index() {
+export default function Slides() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
             title={`${siteConfig.title}`}
             description="Universal tool for DevOps and Cloud Automation">
-            <Header/>
             <main>
-                <section className={styles.features}>
+                <section className={styles.slidesContainer}>
                     <div className="container">
                         <ImageGallery items={images}
                                       thumbnailPosition={"left"}
