da7de8acfbead1d29b6f946fef04d9f6e89a9ef2
aknysh <andriy.knysh@gmail.com>
2022-11-10 20:06:24 -0500

    Updates

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
-import Content from "./index-content.mdx";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css"
 
-function HomepageHeader() {
+function Header() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <header className={clsx("hero", styles.heroBanner)}>
@@ -18,16 +18,41 @@ function HomepageHeader() {
     );
 }
 
-export default function Home() {
+// https://github.com/xiaolin/react-image-gallery
+// https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
+const images = Array.from({length: 27}, (_, i) => {
+        let ix = i + 1;
+        return {
+            original: "/img/slides/atmos-intro-" + ix + ".svg",
+            thumbnail: "/img/slides/atmos-intro-" + ix + ".svg",
+            originalAlt: "Atmos introduction slide " + ix,
+            originalTitle: "Atmos introduction slide " + ix,
+            thumbnailAlt: "Atmos introduction slide " + ix,
+            thumbnailTitle: "Atmos introduction slide " + ix,
+            loading: "lazy"
+        }
+    }
+);
+
+export default function Index() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
             title={`${siteConfig.title}`}
             description="Universal tool for DevOps and Cloud Automation">
-            <HomepageHeader/>
+            <Header/>
             <main>
-                <Content></Content>
-                <HomepageFeatures/>
+                <section className={styles.features}>
+                    <div className="container">
+                        <ImageGallery items={images}
+                                      thumbnailPosition={"left"}
+                                      showBullets={false}
+                                      showIndex={true}
+                                      slideInterval={4000}
+                                      lazyLoad={true}
+                        />
+                    </div>
+                </section>
             </main>
         </Layout>
     );
