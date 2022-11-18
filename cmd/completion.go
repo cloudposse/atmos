a558ad2de9306f19d0189563c204aa99e0503860
aknysh <andriy.knysh@gmail.com>
2022-11-18 01:37:40 -0500

    Update docs

--- cmd/completion.go
+++ cmd/completion.go
@@ -1,28 +1,45 @@
 package cmd
 
 import (
+	"github.com/spf13/cobra"
 	"os"
 
-	"github.com/spf13/cobra"
+	u "github.com/cloudposse/atmos/pkg/utils"
 )
 
-var CompletionCmd = &cobra.Command{
+var completionCmd = &cobra.Command{
 	Use:                   "completion [bash|zsh|fish|powershell]",
-	Short:                 "Generate completion script",
-	Long:                  "To load completions",
+	Short:                 "Generate completion script for Bash, Zsh, fish or PowerShell",
+	Long:                  "This command generates completion scripts for Bash, Zsh, fish and PowerShell",
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.ExactValidArgs(1),
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		switch args[0] {
 		case "bash":
-			cmd.Root().GenBashCompletion(os.Stdout)
+			err := cmd.Root().GenBashCompletion(os.Stdout)
+			if err != nil {
+				u.PrintErrorToStdErrorAndExit(err)
+			}
 		case "zsh":
-			cmd.Root().GenZshCompletion(os.Stdout)
+			err := cmd.Root().GenZshCompletion(os.Stdout)
+			if err != nil {
+				u.PrintErrorToStdErrorAndExit(err)
+			}
 		case "fish":
-			cmd.Root().GenFishCompletion(os.Stdout, true)
+			err := cmd.Root().GenFishCompletion(os.Stdout, true)
+			if err != nil {
+				u.PrintErrorToStdErrorAndExit(err)
+			}
 		case "powershell":
-			cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+			err := cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+			if err != nil {
+				u.PrintErrorToStdErrorAndExit(err)
+			}
 		}
 	},
+}
+
+func init() {
+	RootCmd.AddCommand(completionCmd)
 }
