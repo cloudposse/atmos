3f526a33911d987e11478d99d53642fcf276ed29
aknysh <andriy.knysh@gmail.com>
2022-11-18 00:14:12 -0500

    Update dependencies calculation

--- pkg/describe/terraform_describe_component_test.go
+++ pkg/describe/terraform_describe_component_test.go
@@ -2,16 +2,12 @@ package describe
 
 import (
 	e "github.com/cloudposse/atmos/internal/exec"
-	cfg "github.com/cloudposse/atmos/pkg/config"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v2"
 	"testing"
 )
 
 func TestDescribeComponent(t *testing.T) {
-	_, err := cfg.InitCliConfig(cfg.ConfigAndStacksInfo{}, true)
-	assert.Nil(t, err)
-
 	component := "test/test-component-override-3"
 	stack := "tenant1-ue2-dev"
 
