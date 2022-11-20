76752bf8b681cbac2d4eba644dd91f6209bd7479
Stoned Elipot <stoned.elipot@gmail.com>
2022-11-20 17:17:29 +0100

    Actually, it's really 'mvdan.cc/sh/v3'

--- internal/exec/shell_utils.go
+++ internal/exec/shell_utils.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	u "github.com/cloudposse/atmos/pkg/utils"
-	"github.com/mvdan/sh/v3/expand"
+	"mvdan.cc/sh/v3/expand"
 	"mvdan.cc/sh/v3/interp"
 	"mvdan.cc/sh/v3/syntax"
 )
