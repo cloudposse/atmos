73661b44c9a4467e1a3cb1336c8d600122cc5007
aknysh <andriy.knysh@gmail.com>
2023-01-13 15:41:32 -0500

    Update `atmos describe affected` command

--- pkg/config/schema.go
+++ pkg/config/schema.go
@@ -339,8 +339,10 @@ type Validation map[string]ValidationItem
 // Affected Atmos components and stacks given two Git commits
 
 type Affected struct {
-	Stack         string `yaml:"stack" json:"stack" mapstructure:"stack"`
-	ComponentType string `yaml:"component_type" json:"component_type" mapstructure:"component_type"`
-	Component     string `yaml:"component" json:"component" mapstructure:"component"`
-	Affected      string `yaml:"affected" json:"affected" mapstructure:"affected"`
+	Stack           string `yaml:"stack" json:"stack" mapstructure:"stack"`
+	ComponentType   string `yaml:"component_type" json:"component_type" mapstructure:"component_type"`
+	Component       string `yaml:"component" json:"component" mapstructure:"component"`
+	Affected        string `yaml:"affected" json:"affected" mapstructure:"affected"`
+	SpaceliftStack  string `yaml:"spacelift_stack,omitempty" json:"spacelift_stack,omitempty" mapstructure:"spacelift_stack"`
+	AtlantisProject string `yaml:"atlantis_project,omitempty" json:"atlantis_project,omitempty" mapstructure:"atlantis_project"`
 }
