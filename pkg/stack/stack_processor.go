e9bbc4ed0d00c6f163845ca4d43040f9926d4e03
aknysh <andriy.knysh@gmail.com>
2023-01-30 12:49:49 -0500

    Support `context` in hierarchical imports

--- pkg/stack/stack_processor.go
+++ pkg/stack/stack_processor.go
@@ -231,6 +231,7 @@ func ProcessYAMLConfigFile(
 		// Support `context` in hierarchical imports.
 		// Deep-merge the parent `context` with the current `context` and propagate the result down the imports chain.
 		// The current `context` takes precedence over the parent `context` and will override items with the same keys.
+		// TODO: instead of calling the conversion functions, we need to switch to generics and update everything to support it
 		listOfMaps := []map[any]any{c.MapsOfStringsToMapsOfInterfaces(context), c.MapsOfStringsToMapsOfInterfaces(importStruct.Context)}
 		mergedContext, err := m.Merge(listOfMaps)
 		if err != nil {
