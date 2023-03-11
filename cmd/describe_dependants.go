b1745de0568bbf2ead3eac199a6d73b6e352e25d
aknysh <andriy.knysh@gmail.com>
2023-03-11 17:34:26 -0500

    Add `atmos describe dependants` command

---
+++ cmd/describe_dependants.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	e "github.com/cloudposse/atmos/internal/exec"
+	u "github.com/cloudposse/atmos/pkg/utils"
+)
+
+// describeAffectedCmd produces a list of the affected Atmos components and stacks given two Git commits
+var describeDependantsCmd = &cobra.Command{
+	Use:                "dependants",
+	Short:              "Execute 'describe dependants' command",
+	Long:               `This command produces a list of Atmos components in Atmos stacks that depend on the provided Atmos component: atmos describe dependants [options]`,
+	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: false},
+	Run: func(cmd *cobra.Command, args []string) {
+		err := e.ExecuteDescribeDependantsCmd(cmd, args)
+		if err != nil {
+			u.PrintErrorToStdErrorAndExit(err)
+		}
+	},
+}
+
+func init() {
+	describeDependantsCmd.DisableFlagParsing = false
+
+	describeDependantsCmd.PersistentFlags().StringP("stack", "s", "", "atmos describe dependants <component> -s <stack>")
+	describeDependantsCmd.PersistentFlags().StringP("format", "f", "yaml", "The output format: atmos describe dependants <component> -s <stack> --format=yaml|json ('yaml' is default)")
+	describeDependantsCmd.PersistentFlags().String("file", "", "Write the result to the file: atmos describe dependants <component> -s <stack> --file dependants.yaml")
+
+	err := describeDependantsCmd.MarkPersistentFlagRequired("stack")
+	if err != nil {
+		u.PrintErrorToStdErrorAndExit(err)
+	}
+
+	describeCmd.AddCommand(describeDependantsCmd)
+}
---
+++ internal/exec/describe_dependants.go
@@ -0,0 +1,83 @@
+package exec
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	cfg "github.com/cloudposse/atmos/pkg/config"
+	u "github.com/cloudposse/atmos/pkg/utils"
+)
+
+// ExecuteDescribeDependantsCmd executes `describe dependants` command
+func ExecuteDescribeDependantsCmd(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return errors.New("invalid arguments. The command requires one argument `component`")
+	}
+
+	flags := cmd.Flags()
+
+	stack, err := flags.GetString("stack")
+	if err != nil {
+		return err
+	}
+
+	format, err := flags.GetString("format")
+	if err != nil {
+		return err
+	}
+
+	file, err := flags.GetString("file")
+	if err != nil {
+		return err
+	}
+
+	component := args[0]
+
+	componentSection, err := ExecuteDescribeDependants(component, stack)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println()
+	err = printOrWriteToFile(format, file, componentSection)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ExecuteDescribeDependants produces a list of Atmos components in Atmos stacks that depend on the provided Atmos component
+func ExecuteDescribeDependants(component string, stack string) (map[string]any, error) {
+	var configAndStacksInfo cfg.ConfigAndStacksInfo
+	configAndStacksInfo.ComponentFromArg = component
+	configAndStacksInfo.Stack = stack
+
+	cliConfig, err := cfg.InitCliConfig(configAndStacksInfo, true)
+	if err != nil {
+		return nil, err
+	}
+
+	configAndStacksInfo.ComponentType = "terraform"
+	configAndStacksInfo, err = ProcessStacks(cliConfig, configAndStacksInfo, true)
+	if err != nil {
+		u.PrintErrorVerbose(cliConfig.Logs.Verbose, err)
+		configAndStacksInfo.ComponentType = "helmfile"
+		configAndStacksInfo, err = ProcessStacks(cliConfig, configAndStacksInfo, true)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Add Atmos component and stack
+	configAndStacksInfo.ComponentSection["atmos_component"] = configAndStacksInfo.ComponentFromArg
+	configAndStacksInfo.ComponentSection["atmos_stack"] = configAndStacksInfo.StackFromArg
+
+	// If the command-line component does not inherit anything, then the Terraform/Helmfile component is the same as the provided one
+	if comp, ok := configAndStacksInfo.ComponentSection["component"].(string); !ok || comp == "" {
+		configAndStacksInfo.ComponentSection["component"] = configAndStacksInfo.ComponentFromArg
+	}
+
+	return configAndStacksInfo.ComponentSection, nil
+}
