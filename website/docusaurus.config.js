109d0832d193bc6faa9512fde5311dabd9a8bdf4
Zinovii Dmytriv <zinovii@gmail.com>
2023-03-24 18:55:39 +0200

    Consolidated index

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -123,7 +123,7 @@ const config = {
             algolia: {
                 appId: process.env.ALGOLIA_APP_ID || '32YOERUX83',
                 apiKey: process.env.ALGOLIA_SEARCH_API_KEY || '557985309adf0e4df9dcf3cb29c61928', // this is SEARCH ONLY API key and is not sensitive information
-                indexName: process.env.ALGOLIA_INDEX_NAME || 'atmos.tools',
+                indexName: process.env.ALGOLIA_INDEX_NAME || 'docs.cloudposse.com',
                 contextualSearch: false
             },
         }),
