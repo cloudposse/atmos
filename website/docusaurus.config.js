38b8fe6ca1221e16f0b969815bddfe98928cd1b5
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-03-24 23:39:42 +0200

    Algolia to redirect to external site when search results from external site (#353)
    Co-authored-by: Zinovii Dmytriv <zinovii@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -124,6 +124,7 @@ const config = {
                 appId: process.env.ALGOLIA_APP_ID || '32YOERUX83',
                 apiKey: process.env.ALGOLIA_SEARCH_API_KEY || '557985309adf0e4df9dcf3cb29c61928', // this is SEARCH ONLY API key and is not sensitive information
                 indexName: process.env.ALGOLIA_INDEX_NAME || 'docs.cloudposse.com',
+                externalUrlRegex: 'docs\\.cloudposse\\.com',
                 contextualSearch: false
             },
         }),
