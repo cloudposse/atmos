8c235a2f7984ea163748545cedb61342e1303faf
aknysh <andriy.knysh@gmail.com>
2023-10-19 09:36:54 -0400

    Add `vendor.yaml`

--- internal/exec/utils.go
+++ internal/exec/utils.go
@@ -3,7 +3,6 @@ package exec
 import (
 	"errors"
 	"fmt"
-	"os"
 	"sort"
 	"strings"
 
@@ -829,54 +828,6 @@ func generateComponentBackendConfig(backendType string, backendConfig map[any]an
 				backendType: backendConfig,
 			},
 		},
-	}
-}
-
-// printOrWriteToFile takes the output format (`yaml` or `json`) and a file name,
-// and prints the data to the console or to a file (if file is specified)
-func printOrWriteToFile(
-	format string,
-	file string,
-	data any,
-) error {
-	switch format {
-	case "yaml":
-		if file == "" {
-			err := u.PrintAsYAML(data)
-			if err != nil {
-				return err
-			}
-		} else {
-			err := u.WriteToFileAsYAML(file, data, 0644)
-			if err != nil {
-				return err
-			}
-		}
-
-	case "json":
-		if file == "" {
-			err := u.PrintAsJSON(data)
-			if err != nil {
-				return err
-			}
-		} else {
-			err := u.WriteToFileAsJSON(file, data, 0644)
-			if err != nil {
-				return err
-			}
-		}
-
-	default:
-		return fmt.Errorf("invalid 'format': %s", format)
-	}
-
-	return nil
-}
-
-func removeTempDir(cliConfig schema.CliConfiguration, path string) {
-	err := os.RemoveAll(path)
-	if err != nil {
-		u.LogWarning(cliConfig, err.Error())
 	}
 }
 
