c089cd5d3533aea9f9313d3c11759ef75e51aa2c
aknysh <andriy.knysh@gmail.com>
2023-12-01 12:48:53 -0500

    Update docs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -33,6 +33,12 @@ const config = {
         locales: ['en'],
     },
 
+    plugins: [
+        [
+            'docusaurus-plugin-image-zoom', {},
+        ]
+    ],
+
     presets: [
         [
             'classic',
@@ -143,6 +149,16 @@ const config = {
                 indexName: process.env.ALGOLIA_INDEX_NAME || 'atmos.tools',
                 contextualSearch: false
             },
+            zoom: {
+                selector: '.markdown :not(em) > img',
+                config: {
+                    // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+                    background: {
+                        light: 'rgb(255, 255, 255)',
+                        dark: 'rgb(50, 50, 50)'
+                    }
+                }
+            }
         }),
 
     markdown: {
