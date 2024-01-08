972af62823ca0bbe36662e1abf4a06c6319c8b20
aknysh <andriy.knysh@gmail.com>
2024-01-08 13:34:28 -0500

    Updates

--- internal/exec/exec.go
+++ internal/exec/exec.go
@@ -1,18 +1,13 @@
 package exec
 
 import (
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 
 	tui "github.com/cloudposse/atmos/internal/tui/stack_component_select"
 )
 
 func ExecuteExecCmd(cmd *cobra.Command, args []string) error {
-	board := tui.NewBoard()
-	board.InitLists()
-	p := tea.NewProgram(board)
-
-	if _, err := p.Run(); err != nil {
+	if err := tui.Execute(); err != nil {
 		return err
 	}
 
---
+++ internal/tui/stack_component_select/tui.go
@@ -0,0 +1,17 @@
+package stack_component_select
+
+import tea "github.com/charmbracelet/bubbletea"
+
+// Execute starts the TUI app
+func Execute() error {
+	board = NewBoard()
+	board.InitLists()
+	p := tea.NewProgram(board)
+
+	if _, err := p.Run(); err != nil {
+		return err
+	}
+
+	return nil
+
+}
