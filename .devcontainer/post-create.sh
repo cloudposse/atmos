b2fe3532195afa268d50e6528dab2e0b8304a00f
Erik Osterman <erik@cloudposse.com>
2024-06-13 09:49:50 -0500

    enhance docker compose for ux

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,6 @@
 
 # Install a .envrc file in each example directory (it's ignored in .gitignore)
 find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \;
+
+# Start localstack in the background, sincen it can take a little bit to start up
+docker compose up -d -f /workspace/examples/demo-localstack/docker-compose.yml
