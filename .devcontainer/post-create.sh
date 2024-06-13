4db8c913f94c104568b2eebaca269021f5fc880c
Erik Osterman <erik@cloudposse.com>
2024-06-13 09:55:53 -0500

    fix command invocation

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,4 +4,4 @@
 find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \;
 
 # Start localstack in the background, sincen it can take a little bit to start up
-docker compose up -d -f /workspace/examples/demo-localstack/docker-compose.yml
+docker compose -f /workspace/examples/demo-localstack/docker-compose.yml up -d
