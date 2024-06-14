08ea737286c3458b058a00907c462d82ee3d2439
Erik Osterman <erik@cloudposse.com>
2024-06-14 02:47:47 -0500

    set log files

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,10 +10,15 @@ mv /workspace/examples/welcome.md /workspace/examples/README.md
 # Note that this also means errors won’t fail the bootstrapping of the container, which can mask issues.
 
 # Install a .envrc file in each example directory (it's ignored in .gitignore)
-screen -S direnv-setup "find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \;"
+screen -L -Logfile /tmp/direnv.log -S direnv-setup -dm \
+	sh -c "find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \;"
 
 # Start localstack in the background, since it can take a little bit to start up
-screen -S localstack -dm 'docker compose -f /workspace/examples/demo-localstack/docker-compose.yml up'
+cd /workspace/examples/demo-localstack
+screen -L -Logfile /tmp/localstack.log -S localstack -dm sh -c 'docker compose up'
 
 # Start k3s in the background, since it can take a little bit to start up
-screen -S k3s -dm 'docker compose -f /workspace/examples/demo-helmfile/docker-compose.yml up'
+# Note, it will mount . to the container and write the kubeconfig.yaml file
+# This should used as the file for KUBECONFIG
+cd /workspace/examples/demo-helmfile
+screen -L -Logfile /tmp/k3s.log -S k3s -dm sh -c 'docker compose up'
