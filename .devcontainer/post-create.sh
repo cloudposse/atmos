4301ec018be73f49d1685b89e87040cc5db033e2
Erik Osterman <erik@cloudposse.com>
2024-06-14 00:00:05 -0500

    add a working k3s

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
+# Use nohup to run the following commands in the background so they don’t block startup time
+# Note that this also means errors won’t fail the bootstrapping of the container, which can mask issues.
+
 # Install a .envrc file in each example directory (it's ignored in .gitignore)
-find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \;
+nohup find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \; &
 
 # Start localstack in the background, sincen it can take a little bit to start up
-docker compose -f /workspace/examples/demo-localstack/docker-compose.yml up -d
+nohup docker compose -f /workspace/examples/demo-localstack/docker-compose.yml up -d &
+
+# Celebrate! 🎉
+timeout 3 confetty
