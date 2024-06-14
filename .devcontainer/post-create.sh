a3f1f2d06adb1d64b95046dba05a88952386d13c
Erik Osterman <erik@cloudposse.com>
2024-06-14 01:01:39 -0500

    abandoned the restore terminals approach

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,13 +1,18 @@
 #!/bin/zsh
 
+set +x
+
+# Display a different welcome message for Codespaces
+mv /workspace/examples/welcome.md /workspace/examples/README.md
+
 # Use nohup to run the following commands in the background so they don’t block startup time
 # Note that this also means errors won’t fail the bootstrapping of the container, which can mask issues.
 
 # Install a .envrc file in each example directory (it's ignored in .gitignore)
-nohup find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \; &
+nohup find /workspace/examples -mindepth 1 -type d -exec sh -c 'echo show_readme > {}/.envrc' \; >/tmp/direnv.log&
 
 # Start localstack in the background, sincen it can take a little bit to start up
-nohup docker compose -f /workspace/examples/demo-localstack/docker-compose.yml up -d &
+nohup docker compose -f /workspace/examples/demo-localstack/docker-compose.yml up -d >/tmp/direnv.log &
 
 # Celebrate! 🎉
 timeout --preserve-status 3 confetty
