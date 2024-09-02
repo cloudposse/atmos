8d12f3c4fe91740e40bd6afe770e46394048ec7c
aknysh <andriy.knysh@gmail.com>
2024-09-01 22:24:20 -0400

    updates

--- pkg/convert/yaml.go
+++ pkg/convert/yaml.go
@@ -4,7 +4,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// YAMLToMapOfStrings takes a YAML string as input and returns a map[string]any
+// YAMLToMapOfStrings takes a YAML string as input and unmarshals it into a map[string]any
 func YAMLToMapOfStrings(input string) (map[string]any, error) {
 	var data map[string]any
 	byt := []byte(input)
