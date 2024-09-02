37c40e770968e99809bea1da002070fee5e6a648
aknysh <andriy.knysh@gmail.com>
2024-09-02 09:15:24 -0400

    updates

--- pkg/convert/json.go
+++ pkg/convert/json.go
@@ -1,10 +1,14 @@
 package convert
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/cloudposse/atmos/pkg/schema"
+)
 
 // JSONToMapOfInterfaces takes a JSON string as input and returns a map[string]any
-func JSONToMapOfInterfaces(input string) (map[string]any, error) {
-	var data map[string]any
+func JSONToMapOfInterfaces(input string) (schema.AtmosSectionMapType, error) {
+	var data schema.AtmosSectionMapType
 	byt := []byte(input)
 
 	if err := json.Unmarshal(byt, &data); err != nil {
