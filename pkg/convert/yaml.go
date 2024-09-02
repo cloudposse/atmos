287afe41201eaca4c81e21ceb1af635625de7484
aknysh <andriy.knysh@gmail.com>
2024-09-02 09:10:05 -0400

    updates

--- pkg/convert/yaml.go
+++ pkg/convert/yaml.go
@@ -1,8 +1,6 @@
 package convert
 
 import (
-	"fmt"
-
 	"gopkg.in/yaml.v3"
 
 	"github.com/cloudposse/atmos/pkg/schema"
@@ -17,27 +15,4 @@ func YAMLToMapOfStrings(input string) (schema.AtmosSectionMapType, error) {
 		return nil, err
 	}
 	return data, nil
-}
-
-type CustomType struct {
-	Value string
-}
-
-func (c CustomType) MarshalYAML() (any, error) {
-	// Return a YAML node or a plain value.
-	node := yaml.Node{
-		Kind:  yaml.ScalarNode,
-		Tag:   "!!str",
-		Value: "custom:" + c.Value,
-	}
-	return &node, nil
-}
-
-// UnmarshalYAML implements the yaml.Unmarshaler interface
-func (c *CustomType) UnmarshalYAML(value *yaml.Node) error {
-	if value.Kind == yaml.ScalarNode && value.Tag == "!!str" && len(value.Value) > 7 && value.Value[:7] == "custom:" {
-		c.Value = value.Value[7:]
-		return nil
-	}
-	return fmt.Errorf("unexpected YAML value: %v", value.Value)
 }
