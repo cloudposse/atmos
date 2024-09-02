28dffd340aabbbbfea3606c1b05f853ed0be1327
aknysh <andriy.knysh@gmail.com>
2024-09-01 23:07:56 -0400

    updates

--- pkg/convert/yaml.go
+++ pkg/convert/yaml.go
@@ -10,10 +10,10 @@ import (
 
 // YAMLToMapOfStrings takes a YAML string as input and unmarshals it into a map[string]any
 func YAMLToMapOfStrings(input string) (schema.AtmosSectionMapType, error) {
-	var data map[string]any
-	byt := []byte(input)
+	var data schema.AtmosSectionMapType
+	b := []byte(input)
 
-	if err := yaml.Unmarshal(byt, &data); err != nil {
+	if err := yaml.Unmarshal(b, &data); err != nil {
 		return nil, err
 	}
 	return data, nil
