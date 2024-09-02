b82cf461f2ed4e23e7a580e77fbeac4a8c8182e2
aknysh <andriy.knysh@gmail.com>
2024-09-01 22:57:41 -0400

    updates

--- pkg/convert/yaml.go
+++ pkg/convert/yaml.go
@@ -1,11 +1,15 @@
 package convert
 
 import (
+	"fmt"
+
 	"gopkg.in/yaml.v3"
+
+	"github.com/cloudposse/atmos/pkg/schema"
 )
 
 // YAMLToMapOfStrings takes a YAML string as input and unmarshals it into a map[string]any
-func YAMLToMapOfStrings(input string) (map[string]any, error) {
+func YAMLToMapOfStrings(input string) (schema.AtmosSectionMapType, error) {
 	var data map[string]any
 	byt := []byte(input)
 
@@ -13,4 +17,27 @@ func YAMLToMapOfStrings(input string) (map[string]any, error) {
 		return nil, err
 	}
 	return data, nil
+}
+
+type CustomType struct {
+	Value string
+}
+
+func (c CustomType) MarshalYAML() (any, error) {
+	// Return a YAML node or a plain value.
+	node := yaml.Node{
+		Kind:  yaml.ScalarNode,
+		Tag:   "!!str",
+		Value: "custom:" + c.Value,
+	}
+	return &node, nil
+}
+
+// UnmarshalYAML implements the yaml.Unmarshaler interface
+func (c *CustomType) UnmarshalYAML(value *yaml.Node) error {
+	if value.Kind == yaml.ScalarNode && value.Tag == "!!str" && len(value.Value) > 7 && value.Value[:7] == "custom:" {
+		c.Value = value.Value[7:]
+		return nil
+	}
+	return fmt.Errorf("unexpected YAML value: %v", value.Value)
 }
