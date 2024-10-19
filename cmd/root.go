c866ab57b3ca79e6849d4c3c53c3df58d58584d2
Vinicius C. <vinny.cardoso96@gmail.com>
2024-10-19 15:25:36 +0100

    Fix: condition to display help interactive menu (#724)
    * fix: condition to display help interactive menu
    
    * chore: clean code improve readability
    
    * fix: change flag in help command
    
    * Add `--stack` flag to `atmos describe affected` command
    
    ---------
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>
    Co-authored-by: aknysh <andriy.knysh@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,21 @@ var RootCmd = &cobra.Command{
 	Use:   "atmos",
 	Short: "Universal Tool for DevOps and Cloud Automation",
 	Long:  `Atmos is a universal tool for DevOps and cloud automation used for provisioning, managing and orchestrating workflows across various toolchains`,
-	PreRun: func(cmd *cobra.Command, args []string) {
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// Determine if the command is a help command or if the help flag is set
+		isHelpCommand := cmd.Name() == "help"
+		helpFlag := cmd.Flags().Changed("help")
+
+		isHelpRequested := isHelpCommand || helpFlag
+
+		if isHelpRequested {
+			// Do not silence usage or errors when help is invoked
+			cmd.SilenceUsage = false
+			cmd.SilenceErrors = false
+		} else {
+			cmd.SilenceUsage = true
+			cmd.SilenceErrors = true
+		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		// Check Atmos configuration
