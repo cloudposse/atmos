0a5122aadcda4051e2d73fc5b52f773db9f187dd
RoseSecurity <michaeljrosenfeld22@gmail.com>
2024-11-01 10:13:43 -0400

    feat: dynamically detect terminal width

--- cmd/docs.go
+++ cmd/docs.go
@@ -10,6 +10,7 @@ import (
 	"github.com/charmbracelet/glamour"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 
 	cfg "github.com/cloudposse/atmos/pkg/config"
 	"github.com/cloudposse/atmos/pkg/schema"
@@ -38,6 +39,24 @@ var docsCmd = &cobra.Command{
 			cliConfig, err := cfg.InitCliConfig(info, true)
 			if err != nil {
 				u.LogErrorAndExit(schema.CliConfiguration{}, err)
+			}
+
+			// Detect terminal width if width flag is not set
+			if !cmd.Flags().Changed("width") {
+				if term.IsTerminal(int(os.Stdout.Fd())) {
+					w, _, err := term.GetSize(int(os.Stdout.Fd()))
+					if err == nil {
+						width = uint(w)
+					}
+
+					if width > 120 {
+						width = 120
+					}
+
+					if width == 0 {
+						width = 80
+					}
+				}
 			}
 
 			// Construct the full path to the Terraform component by combining the Atmos base path, Terraform base path, and component name
@@ -104,5 +123,5 @@ var docsCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(docsCmd)
-	docsCmd.Flags().UintVarP(&width, "width", "w", 80, "word-wrap at width")
+	docsCmd.Flags().UintVarP(&width, "width", "w", 0, "word-wrap at width (set to 0 to disable)")
 }
--- go.mod
+++ go.mod
@@ -41,6 +41,7 @@ require (
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
 	github.com/zclconf/go-cty v1.15.0
+	golang.org/x/term v0.25.0
 	gopkg.in/yaml.v3 v3.0.1
 	mvdan.cc/sh/v3 v3.10.0
 )
@@ -242,7 +243,6 @@ require (
 	golang.org/x/oauth2 v0.22.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/term v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
