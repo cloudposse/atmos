a6dc100241a78872ddacad39ff35be005e36a7da
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2024-11-03 21:23:26 +0600

    add atmos in PS1
    Signed-off-by: Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>

--- internal/exec/shell_utils.go
+++ internal/exec/shell_utils.go
@@ -154,6 +154,7 @@ func execTerraformShellCommand(
 	componentEnvList = append(componentEnvList, fmt.Sprintf("TF_CLI_ARGS_import=-var-file=%s", varFile))
 	componentEnvList = append(componentEnvList, fmt.Sprintf("TF_CLI_ARGS_destroy=-var-file=%s", varFile))
 	componentEnvList = append(componentEnvList, fmt.Sprintf("TF_CLI_ARGS_console=-var-file=%s", varFile))
+	componentEnvList = append(componentEnvList, "PS1=atmos>")
 
 	u.LogDebug(cliConfig, "\nStarting a new interactive shell where you can execute all native Terraform commands (type 'exit' to go back)")
 	u.LogDebug(cliConfig, fmt.Sprintf("Component: %s\n", component))
@@ -187,7 +188,6 @@ func execTerraformShellCommand(
 			}
 			shellCommand = bashPath
 		}
-		shellCommand = shellCommand + " -l"
 	}
 
 	u.LogDebug(cliConfig, fmt.Sprintf("Starting process: %s\n", shellCommand))
