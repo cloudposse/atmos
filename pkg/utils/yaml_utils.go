ce0633795738a180d3a006836e8469b30215161f
aknysh <andriy.knysh@gmail.com>
2024-11-28 22:36:05 -0500

    updates

--- pkg/utils/yaml_utils.go
+++ pkg/utils/yaml_utils.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -57,67 +56,6 @@ func ConvertToYAML(data any) (string, error) {
 	return string(y), nil
 }
 
-// TaggedValue holds both the value and the YAML tag for each node.
-type TaggedValue struct {
-	Tag   string `yaml:"tag"`
-	Value any    `yaml:"value"`
-}
-
-// parseNodeWithTags recursively extracts values and tags from yaml.Node and stores them in a map.
-func parseNodeWithTags(node *yaml.Node, out map[string]TaggedValue) error {
-	// Handle DocumentNode by unwrapping its content
-	if node.Kind == yaml.DocumentNode && len(node.Content) > 0 {
-		return parseNodeWithTags(node.Content[0], out)
-	}
-
-	if node.Kind != yaml.MappingNode {
-		return fmt.Errorf("expected a mapping node at the root")
-	}
-
-	// Process nodes in key-value pairs
-	for i := 0; i < len(node.Content); i += 2 {
-		keyNode := node.Content[i]
-		valueNode := node.Content[i+1]
-
-		// Create TaggedValue to store the tag and value
-		taggedValue := TaggedValue{Tag: valueNode.Tag}
-
-		switch valueNode.Kind {
-		case yaml.ScalarNode:
-			// Directly store scalar values
-			taggedValue.Value = valueNode.Tag + " " + valueNode.Value
-		case yaml.MappingNode:
-			// Recursively parse maps
-			nestedMap := make(map[string]TaggedValue)
-			if err := parseNodeWithTags(valueNode, nestedMap); err != nil {
-				return err
-			}
-			taggedValue.Value = nestedMap
-		case yaml.SequenceNode:
-			// Parse sequences
-			var sequence []TaggedValue
-			for _, seqNode := range valueNode.Content {
-				seqItem := TaggedValue{Tag: seqNode.Tag, Value: seqNode.Value}
-				sequence = append(sequence, seqItem)
-			}
-			taggedValue.Value = sequence
-		case yaml.DocumentNode:
-			// Handle nested DocumentNode
-			nestedMap := make(map[string]TaggedValue)
-			if err := parseNodeWithTags(valueNode, nestedMap); err != nil {
-				return err
-			}
-			taggedValue.Value = nestedMap
-		default:
-			return fmt.Errorf("invalid YAML node type")
-		}
-
-		// Assign parsed TaggedValue to output map with key
-		out[keyNode.Value] = taggedValue
-	}
-	return nil
-}
-
 func processCustomTags(node *yaml.Node) error {
 	if node.Kind == yaml.DocumentNode && len(node.Content) > 0 {
 		return processCustomTags(node.Content[0])
@@ -167,14 +105,3 @@ func UnmarshalYAML[T any](input string) (T, error) {
 
 	return data, nil
 }
-
-//resultWithTags := make(map[string]TaggedValue)
-//if err := parseNodeWithTags(&node, resultWithTags); err != nil {
-//	return zeroValue, err
-//}
-//
-//_, err := ConvertToYAML(resultWithTags)
-//if err != nil {
-//	return zeroValue, err
-//}
-// gopkg.in/yaml.v3.TaggedStyle
