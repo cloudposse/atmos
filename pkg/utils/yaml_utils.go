6cc90b8127b18edcd55728432851f144d1444c4c
aknysh <andriy.knysh@gmail.com>
2024-11-28 22:22:00 -0500

    updates

--- pkg/utils/yaml_utils.go
+++ pkg/utils/yaml_utils.go
@@ -9,6 +9,12 @@ import (
 	"github.com/cloudposse/atmos/pkg/schema"
 )
 
+var (
+	AtmosYamlTags = []string{
+		"!terraform.output",
+	}
+)
+
 // PrintAsYAML prints the provided value as YAML document to the console
 func PrintAsYAML(data any) error {
 	y, err := ConvertToYAML(data)
@@ -112,27 +118,26 @@ func parseNodeWithTags(node *yaml.Node, out map[string]TaggedValue) error {
 	return nil
 }
 
-// gopkg.in/yaml.v3.TaggedStyle
-func processNode(node *yaml.Node) error {
+func processCustomTags(node *yaml.Node) error {
 	if node.Kind == yaml.DocumentNode && len(node.Content) > 0 {
-		return processNode(node.Content[0])
+		return processCustomTags(node.Content[0])
 	}
 
 	for i := 0; i < len(node.Content); i += 2 {
 		valueNode := node.Content[i+1]
 
-		if valueNode.Tag == "!terraform.output" || valueNode.Tag == "!template" {
+		if SliceContainsString(AtmosYamlTags, valueNode.Tag) {
 			valueNode.Value = valueNode.Tag + " " + valueNode.Value
 		}
 
 		if valueNode.Kind == yaml.SequenceNode {
 			for _, seqNode := range valueNode.Content {
-				if err := processNode(seqNode); err != nil {
+				if err := processCustomTags(seqNode); err != nil {
 					return err
 				}
 			}
 		} else {
-			if err := processNode(valueNode); err != nil {
+			if err := processCustomTags(valueNode); err != nil {
 				return err
 			}
 		}
@@ -145,26 +150,16 @@ func UnmarshalYAML[T any](input string) (T, error) {
 	var node yaml.Node
 	b := []byte(input)
 
-	// Unmarshal into yaml.Node to preserve custom tags and structure
+	// Unmarshal into yaml.Node
 	if err := yaml.Unmarshal(b, &node); err != nil {
 		return zeroValue, err
 	}
 
-	if err := processNode(&node); err != nil {
+	if err := processCustomTags(&node); err != nil {
 		return zeroValue, err
 	}
 
-	//resultWithTags := make(map[string]TaggedValue)
-	//if err := parseNodeWithTags(&node, resultWithTags); err != nil {
-	//	return zeroValue, err
-	//}
-	//
-	//_, err := ConvertToYAML(resultWithTags)
-	//if err != nil {
-	//	return zeroValue, err
-	//}
-
-	// Unmarshal the yaml.Node into the desired type T
+	// Decode the yaml.Node into the desired type T
 	var data T
 	if err := node.Decode(&data); err != nil {
 		return zeroValue, err
@@ -172,3 +167,14 @@ func UnmarshalYAML[T any](input string) (T, error) {
 
 	return data, nil
 }
+
+//resultWithTags := make(map[string]TaggedValue)
+//if err := parseNodeWithTags(&node, resultWithTags); err != nil {
+//	return zeroValue, err
+//}
+//
+//_, err := ConvertToYAML(resultWithTags)
+//if err != nil {
+//	return zeroValue, err
+//}
+// gopkg.in/yaml.v3.TaggedStyle
