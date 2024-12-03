a50939b90d9b3c97d89e00f4e29cd6467c47f626
aknysh <andriy.knysh@gmail.com>
2024-12-02 22:38:03 -0500

    updates

--- internal/exec/yaml_func_terraform_output.go
+++ internal/exec/yaml_func_terraform_output.go
@@ -1,18 +1,12 @@
 package exec
 
 import (
-	"context"
 	"errors"
 	"fmt"
-	"path"
 	"strings"
 	"sync"
 
-	"github.com/hashicorp/terraform-exec/tfexec"
-	"github.com/samber/lo"
-
 	"github.com/cloudposse/atmos/pkg/config"
-	cfg "github.com/cloudposse/atmos/pkg/config"
 	"github.com/cloudposse/atmos/pkg/schema"
 	u "github.com/cloudposse/atmos/pkg/utils"
 )
@@ -55,177 +49,10 @@ func processTagTerraformOutput(cliConfig schema.CliConfiguration, input string) 
 		u.LogErrorAndExit(cliConfig, err)
 	}
 
-	outputProcessed := map[string]any{}
-
-	executable, ok := sections[cfg.CommandSectionName].(string)
-	if !ok {
-		u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' does not have 'command' (executable) defined",
-			input,
-			component,
-			stack,
-		))
-	}
-
-	terraformWorkspace, ok := sections[cfg.WorkspaceSectionName].(string)
-	if !ok {
-		u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' does not have Terraform/OpenTofu workspace defined",
-			input,
-			component,
-			stack,
-		))
-	}
-
-	componentInfo, ok := sections["component_info"]
-	if !ok {
-		u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' does not have 'component_info' defined",
-			input,
-			component,
-			stack,
-		))
-	}
-
-	componentInfoMap, ok := componentInfo.(map[string]any)
-	if !ok {
-		u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' has an invalid 'component_info' section",
-			input,
-			component,
-			stack,
-		))
-	}
-
-	componentPath, ok := componentInfoMap["component_path"].(string)
-	if !ok {
-		u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' has an invalid 'component_info.component_path' section",
-			input,
-			component,
-			stack,
-		))
-	}
-
-	// Auto-generate backend file
-	if cliConfig.Components.Terraform.AutoGenerateBackendFile {
-		backendFileName := path.Join(componentPath, "backend.tf.json")
-
-		u.LogTrace(cliConfig, "\nWriting the backend config to file:")
-		u.LogTrace(cliConfig, backendFileName)
-
-		backendTypeSection, ok := sections["backend_type"].(string)
-		if !ok {
-			u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' has an invalid 'backend_type' section",
-				input,
-				component,
-				stack,
-			))
-		}
-
-		backendSection, ok := sections["backend"].(map[string]any)
-		if !ok {
-			u.LogErrorAndExit(cliConfig, fmt.Errorf("invalid Atmos YAML function: %s\nthe component '%s' in the stack '%s' has an invalid 'backend' section",
-				input,
-				component,
-				stack,
-			))
-		}
-
-		componentBackendConfig, err := generateComponentBackendConfig(backendTypeSection, backendSection, terraformWorkspace)
-		if err != nil {
-			u.LogErrorAndExit(cliConfig, err)
-		}
-
-		err = u.WriteToFileAsJSON(backendFileName, componentBackendConfig, 0644)
-		if err != nil {
-			u.LogErrorAndExit(cliConfig, err)
-		}
-
-		u.LogTrace(cliConfig, "\nWrote the backend config to file:")
-		u.LogTrace(cliConfig, backendFileName)
-	}
-
-	// Generate `providers_override.tf.json` file if the `providers` section is configured
-	providersSection, ok := sections["providers"].(map[string]any)
-
-	if ok && len(providersSection) > 0 {
-		providerOverrideFileName := path.Join(componentPath, "providers_override.tf.json")
-
-		u.LogTrace(cliConfig, "\nWriting the provider overrides to file:")
-		u.LogTrace(cliConfig, providerOverrideFileName)
-
-		var providerOverrides = generateComponentProviderOverrides(providersSection)
-		err = u.WriteToFileAsJSON(providerOverrideFileName, providerOverrides, 0644)
-		if err != nil {
-			u.LogErrorAndExit(cliConfig, err)
-		}
-
-		u.LogTrace(cliConfig, "\nWrote the provider overrides to file:")
-		u.LogTrace(cliConfig, providerOverrideFileName)
-	}
-
-	// Initialize Terraform/OpenTofu
-	tf, err := tfexec.NewTerraform(componentPath, executable)
+	outputProcessed, err := execTerraformOutput(cliConfig, component, stack, sections)
 	if err != nil {
 		u.LogErrorAndExit(cliConfig, err)
 	}
-
-	ctx := context.Background()
-
-	// 'terraform init'
-	// Before executing `terraform init`, delete the `.terraform/environment` file from the component directory
-	cleanTerraformWorkspace(cliConfig, componentPath)
-
-	u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuting 'terraform init %s -s %s'", input, component, stack))
-	err = tf.Init(ctx, tfexec.Upgrade(false))
-	if err != nil {
-		u.LogErrorAndExit(cliConfig, err)
-	}
-	u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuted 'terraform init %s -s %s'", input, component, stack))
-
-	// Terraform workspace
-	u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuting 'terraform workspace new %s' for component '%s' in stack '%s'", input, terraformWorkspace, component, stack))
-
-	err = tf.WorkspaceNew(ctx, terraformWorkspace)
-	if err != nil {
-		u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nterraform workspace exists. Executing 'terraform workspace select %s' for component '%s' in stack '%s'", input, terraformWorkspace, component, stack))
-		err = tf.WorkspaceSelect(ctx, terraformWorkspace)
-		if err != nil {
-			u.LogErrorAndExit(cliConfig, err)
-		}
-		u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuted 'terraform workspace select %s' for component '%s' in stack '%s'", input, terraformWorkspace, component, stack))
-	} else {
-		u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuted 'terraform workspace new %s' for component '%s' in stack '%s'", input, terraformWorkspace, component, stack))
-	}
-
-	// Terraform output
-	u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuting 'terraform output %s -s %s'", input, component, stack))
-
-	outputMeta, err := tf.Output(ctx)
-	if err != nil {
-		u.LogErrorAndExit(cliConfig, err)
-	}
-	u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nexecuted 'terraform output %s -s %s'", input, component, stack))
-
-	if cliConfig.Logs.Level == u.LogLevelTrace {
-		y, err2 := u.ConvertToYAML(outputMeta)
-		if err2 != nil {
-			u.LogError(cliConfig, err2)
-		} else {
-			u.LogTrace(cliConfig, fmt.Sprintf("\nAtmos YAML function: %s\nresult of 'terraform output %s -s %s' before processing it:\n%s\n", input, component, stack, y))
-		}
-	}
-
-	outputProcessed = lo.MapEntries(outputMeta, func(k string, v tfexec.OutputMeta) (string, any) {
-		s := string(v.Value)
-		u.LogTrace(cliConfig, fmt.Sprintf("Atmos YAML function: %s\nconverting the variable '%s' with the value\n%s\nfrom JSON to 'Go' data type\n", input, k, s))
-
-		d, err2 := u.ConvertFromJSON(s)
-
-		if err2 != nil {
-			u.LogError(cliConfig, err2)
-		} else {
-			u.LogTrace(cliConfig, fmt.Sprintf("Atmos YAML function: %s\nconverted the variable '%s' with the value\n%s\nfrom JSON to 'Go' data type\nResult: %v\n", input, k, s, d))
-		}
-
-		return k, d
-	})
 
 	// Cache the result
 	terraformOutputFuncSyncMap.Store(stackSlug, outputProcessed)
