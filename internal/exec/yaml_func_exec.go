08d650dfd6878cd83f844245905b5b5d3f874835
Andriy Knysh <aknysh@users.noreply.github.com>
2024-12-04 10:06:00 -0500

    Introduce Atmos YAML functions (#810)
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * Update website/docs/core-concepts/stacks/yaml-functions/yaml-functions.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update website/docs/core-concepts/stacks/yaml-functions/yaml-functions.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update website/docs/core-concepts/stacks/yaml-functions/yaml-functions.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * Update internal/exec/terraform_utils.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update website/docs/core-concepts/stacks/yaml-functions/yaml-functions.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update website/docs/core-concepts/stacks/yaml-functions/terraform.output.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update website/docs/core-concepts/stacks/yaml-functions/terraform.output.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * updates
    
    * updates
    
    * Update website/docs/core-concepts/stacks/yaml-functions/terraform.output.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update website/docs/core-concepts/stacks/yaml-functions/terraform.output.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * updates
    
    * Update website/docs/core-concepts/stacks/yaml-functions/terraform.output.mdx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * updates
    
    ---------
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ internal/exec/yaml_func_exec.go
@@ -0,0 +1,32 @@
+package exec
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cloudposse/atmos/pkg/config"
+	"github.com/cloudposse/atmos/pkg/schema"
+	u "github.com/cloudposse/atmos/pkg/utils"
+)
+
+func processTagExec(cliConfig schema.CliConfiguration, input string) any {
+	u.LogTrace(cliConfig, fmt.Sprintf("Executing Atmos YAML function: %s", input))
+
+	str, err := getStringAfterTag(cliConfig, input, config.AtmosYamlFuncExec)
+
+	if err != nil {
+		u.LogErrorAndExit(cliConfig, err)
+	}
+
+	res, err := ExecuteShellAndReturnOutput(cliConfig, str, input, ".", nil, false)
+	if err != nil {
+		u.LogErrorAndExit(cliConfig, err)
+	}
+
+	var decoded any
+	if err = json.Unmarshal([]byte(res), &decoded); err != nil {
+		return res
+	}
+
+	return decoded
+}
