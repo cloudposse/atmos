af1687d057ead8a77c5b082271e092a0f2406151
haitham911 <haitham911eg@gmail.com>
2024-12-15 15:05:12 +0200

    add demo-env

---
+++ examples/demo-env/components/terraform/test/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    environment = {
+      source  = "EppO/environment"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
+# Provider declaration for the hypothetical environment provider
+provider "environment" {}
+
+
+# Fetch the required environment variables using the `environment_variables` data source
+data "environment_variables" "required" {
+  filter = "ATMOS_.*" # Fetches all variables starting with "ATMOS_"
+}
+variable "location" {
+  description = "Location for which the weather."
+  type        = string
+  default     = "Los Angeles"
+}
