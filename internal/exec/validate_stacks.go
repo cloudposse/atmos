fb8fa08ed5dfee3da706aef297025abe13fa965a
Matt Calhoun <matt@mattcalhoun.com>
2024-12-16 12:44:18 -0500

    refactor check atmos config code

--- internal/exec/validate_stacks.go
+++ internal/exec/validate_stacks.go
@@ -184,7 +184,6 @@ func createComponentStackMap(
 	var settingsSection map[string]any
 	var envSection map[string]any
 	var providersSection map[string]any
-	var hooksSection map[string]any
 	var overridesSection map[string]any
 	var backendSection map[string]any
 	var backendTypeSection string
@@ -223,10 +222,6 @@ func createComponentStackMap(
 						providersSection = map[string]any{}
 					}
 
-					if hooksSection, ok = componentSection[cfg.HooksSectionName].(map[string]any); !ok {
-						hooksSection = map[string]any{}
-					}
-
 					if overridesSection, ok = componentSection[cfg.OverridesSectionName].(map[string]any); !ok {
 						overridesSection = map[string]any{}
 					}
@@ -256,7 +251,6 @@ func createComponentStackMap(
 							cfg.SettingsSectionName:    settingsSection,
 							cfg.EnvSectionName:         envSection,
 							cfg.ProvidersSectionName:   providersSection,
-							cfg.HooksSectionName:       hooksSection,
 							cfg.OverridesSectionName:   overridesSection,
 							cfg.BackendSectionName:     backendSection,
 							cfg.BackendTypeSectionName: backendTypeSection,
