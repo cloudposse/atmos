b35863393f5533a4df1bbb02b9d7ca1386eee48d
Matt Calhoun <matt@mattcalhoun.com>
2024-12-20 12:55:59 -0500

    wip commit

--- pkg/store/in_memory_store.go
+++ pkg/store/in_memory_store.go
@@ -2,38 +2,58 @@ package store
 
 import (
 	"fmt"
-	"sync"
 )
 
-// InMemoryStore is an in-memory store implementation.
+// InMemoryStore implements the Store interface with an in-memory map
 type InMemoryStore struct {
 	data map[string]interface{}
-	mu   sync.RWMutex
 }
 
-// Ensure MemoryStore implements the Store interface.
-var _ Store = (*InMemoryStore)(nil)
-
-// NewInMemoryStore initializes a new MemoryStore.
-func NewInMemoryStore(options map[string]interface{}) (Store, error) {
-	return &InMemoryStore{data: make(map[string]interface{})}, nil
+// NewInMemoryStore initializes a new MemoryStore
+func NewInMemoryStore() (Store, error) {
+	return &InMemoryStore{
+		data: make(map[string]interface{}),
+	}, nil
 }
 
-// Set stores a key-value pair in memory.
-func (m *InMemoryStore) Set(key string, value interface{}) error {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.data[key] = value
+// getKey generates a consistent key format for the in-memory store
+func (s *InMemoryStore) getKey(stack, component, key string) string {
+	return fmt.Sprintf("%s/%s/%s", stack, component, key)
+}
+
+// Set stores a value in memory
+func (s *InMemoryStore) Set(stack, component, key string, value interface{}) error {
+	if stack == "" {
+		return fmt.Errorf("stack cannot be empty")
+	}
+	if component == "" {
+		return fmt.Errorf("component cannot be empty")
+	}
+	if key == "" {
+		return fmt.Errorf("key cannot be empty")
+	}
+
+	fullKey := s.getKey(stack, component, key)
+	s.data[fullKey] = value
 	return nil
 }
 
-// Get retrieves a value by key from memory.
-func (m *InMemoryStore) Get(key string) (interface{}, error) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-	value, exists := m.data[key]
+// Get retrieves a value from memory
+func (s *InMemoryStore) Get(stack, component, key string) (interface{}, error) {
+	if stack == "" {
+		return nil, fmt.Errorf("stack cannot be empty")
+	}
+	if component == "" {
+		return nil, fmt.Errorf("component cannot be empty")
+	}
+	if key == "" {
+		return nil, fmt.Errorf("key cannot be empty")
+	}
+
+	fullKey := s.getKey(stack, component, key)
+	value, exists := s.data[fullKey]
 	if !exists {
-		return nil, fmt.Errorf("key '%s' not found", key)
+		return nil, fmt.Errorf("key not found: %s", fullKey)
 	}
 	return value, nil
 }
