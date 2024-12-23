62a9a706392c2996fef622ab953ec996f3300eb8
Erik Osterman <erik@cloudposse.com>
2024-12-23 14:53:04 -0600

    update demo to add docs

--- examples/quick-start-advanced/components/terraform/vpc/remote-state.tf
+++ examples/quick-start-advanced/components/terraform/vpc/remote-state.tf
@@ -1,20 +1,13 @@
 module "vpc_flow_logs_bucket" {
-  count = local.vpc_flow_logs_enabled ? 1 : 0
+  count = var.vpc_flow_logs_enabled ? 1 : 0
 
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
   version = "1.5.0"
 
-  # Specify the Atmos component name (defined in YAML stack config files)
-  # for which to get the remote state outputs
-  component = "vpc-flow-logs-bucket"
-
-  # Override the context variables to point to a different Atmos stack if the
-  # `vpc-flow-logs-bucket` Atmos component is provisioned in another AWS account, OU or region
-  stage       = try(coalesce(var.vpc_flow_logs_bucket_stage_name, module.this.stage), null)
-  environment = try(coalesce(var.vpc_flow_logs_bucket_environment_name, module.this.environment), null)
+  component   = "vpc-flow-logs-bucket"
+  environment = var.vpc_flow_logs_bucket_environment_name
+  stage       = var.vpc_flow_logs_bucket_stage_name
   tenant      = try(coalesce(var.vpc_flow_logs_bucket_tenant_name, module.this.tenant), null)
 
-  # `context` input is a way to provide the information about the stack (using the context
-  # variables `namespace`, `tenant`, `environment`, and `stage` defined in the stack config)
   context = module.this.context
 }
