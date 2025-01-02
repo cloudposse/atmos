6f448037eea0a59b4b1fba4fdd0dab8629f4d51a
Cerebrovinny <vinny.cardoso96@gmail.com>
2025-01-02 20:36:39 +0000

    unify settings

--- pkg/schema/schema.go
+++ pkg/schema/schema.go
@@ -38,8 +38,11 @@ type AtmosConfiguration struct {
 }
 
 type Terminal struct {
-	MaxWidth int  `yaml:"max_width" json:"max_width" mapstructure:"max_width"`
-	Pager    bool `yaml:"pager" json:"pager" mapstructure:"pager"`
+	MaxWidth   int  `yaml:"max_width" json:"max_width" mapstructure:"max_width"`
+	Pager      bool `yaml:"pager" json:"pager" mapstructure:"pager"`
+	Timestamps bool `yaml:"timestamps" json:"timestamps" mapstructure:"timestamps"`
+	Colors     bool `yaml:"colors" json:"colors" mapstructure:"colors"`
+	Unicode    bool `yaml:"unicode" json:"unicode" mapstructure:"unicode"`
 }
 
 type AtmosSettings struct {
