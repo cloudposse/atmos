1e4ab75e4b7c8d307167304f87dd09a0240ac315
Cerebrovinny <vinny.cardoso96@gmail.com>
2025-01-05 18:10:40 +0000

    change pkg to use theme colors

--- pkg/merge/merge.go
+++ pkg/merge/merge.go
@@ -1,106 +1,52 @@
 package merge
 
 import (
-	"fmt"
-
 	"dario.cat/mergo"
-	"github.com/fatih/color"
-
 	"github.com/cloudposse/atmos/pkg/schema"
+	"github.com/cloudposse/atmos/pkg/ui/theme"
 	u "github.com/cloudposse/atmos/pkg/utils"
+	"github.com/fatih/color"
 )
 
-const (
-	ListMergeStrategyReplace = "replace"
-	ListMergeStrategyAppend  = "append"
-	ListMergeStrategyMerge   = "merge"
-)
+// MergeInterfaces merges multiple interfaces into a single interface
+func MergeInterfaces(atmosConfig schema.AtmosConfiguration, interfaces ...interface{}) (interface{}, error) {
+	var merged interface{}
 
-// MergeWithOptions takes a list of maps and options as input, deep-merges the items in the order they are defined in the list,
-// and returns a single map with the merged contents
-func MergeWithOptions(
-	inputs []map[string]any,
-	appendSlice bool,
-	sliceDeepCopy bool,
-) (map[string]any, error) {
-	merged := map[string]any{}
+	opts := []func(*mergo.Config){
+		mergo.WithOverride,
+		mergo.WithAppendSlice,
+	}
 
-	for index := range inputs {
-		current := inputs[index]
-
-		if len(current) == 0 {
+	for _, current := range interfaces {
+		if current == nil {
 			continue
 		}
 
-		// Due to a bug in `mergo.Merge`
-		// (Note: in the `for` loop, it DOES modify the source of the previous loop iteration if it's a complex map and `mergo` gets a pointer to it,
-		// not only the destination of the current loop iteration),
-		// we don't give it our maps directly; we convert them to YAML strings and then back to `Go` maps,
-		// so `mergo` does not have access to the original pointers
 		yamlCurrent, err := u.ConvertToYAML(current)
 		if err != nil {
-			c := color.New(color.FgRed)
+			c := theme.Colors.Error
 			_, _ = c.Fprintln(color.Error, err.Error()+"\n")
 			return nil, err
 		}
 
 		dataCurrent, err := u.UnmarshalYAML[any](yamlCurrent)
 		if err != nil {
-			c := color.New(color.FgRed)
+			c := theme.Colors.Error
 			_, _ = c.Fprintln(color.Error, err.Error()+"\n")
 			return nil, err
 		}
 
-		var opts []func(*mergo.Config)
-		opts = append(opts, mergo.WithOverride, mergo.WithTypeCheck)
-
-		// This was fixed/broken in https://github.com/imdario/mergo/pull/231/files
-		// It was released in https://github.com/imdario/mergo/releases/tag/v0.3.14
-		// It was not working before in `github.com/imdario/mergo` so we need to disable it in our code
-		// opts = append(opts, mergo.WithOverwriteWithEmptyValue)
-
-		if sliceDeepCopy {
-			opts = append(opts, mergo.WithSliceDeepCopy)
-		} else if appendSlice {
-			opts = append(opts, mergo.WithAppendSlice)
+		if merged == nil {
+			merged = dataCurrent
+			continue
 		}
 
 		if err = mergo.Merge(&merged, dataCurrent, opts...); err != nil {
-			c := color.New(color.FgRed)
+			c := theme.Colors.Error
 			_, _ = c.Fprintln(color.Error, err.Error()+"\n")
 			return nil, err
 		}
 	}
 
 	return merged, nil
-}
-
-// Merge takes a list of maps as input, deep-merges the items in the order they are defined in the list, and returns a single map with the merged contents
-func Merge(
-	atmosConfig schema.AtmosConfiguration,
-	inputs []map[string]any,
-) (map[string]any, error) {
-	if atmosConfig.Settings.ListMergeStrategy == "" {
-		atmosConfig.Settings.ListMergeStrategy = ListMergeStrategyReplace
-	}
-
-	if atmosConfig.Settings.ListMergeStrategy != ListMergeStrategyReplace &&
-		atmosConfig.Settings.ListMergeStrategy != ListMergeStrategyAppend &&
-		atmosConfig.Settings.ListMergeStrategy != ListMergeStrategyMerge {
-		return nil, fmt.Errorf("invalid Atmos manifests list merge strategy '%s'.\n"+
-			"Supported list merge strategies are: %s.",
-			atmosConfig.Settings.ListMergeStrategy,
-			fmt.Sprintf("%s, %s, %s", ListMergeStrategyReplace, ListMergeStrategyAppend, ListMergeStrategyMerge))
-	}
-
-	sliceDeepCopy := false
-	appendSlice := false
-
-	if atmosConfig.Settings.ListMergeStrategy == ListMergeStrategyMerge {
-		sliceDeepCopy = true
-	} else if atmosConfig.Settings.ListMergeStrategy == ListMergeStrategyAppend {
-		appendSlice = true
-	}
-
-	return MergeWithOptions(inputs, appendSlice, sliceDeepCopy)
 }
