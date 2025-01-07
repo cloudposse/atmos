8ce9d36c3ce1c1266cfafd39c7f6ad3bba12cad6
Sam Tholiya <tholiya.sam@outlook.in>
2025-01-07 23:13:35 +0100

    Update cmd/validate.go
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- cmd/validate.go
+++ cmd/validate.go
@@ -8,7 +8,7 @@ import (
 var validateCmd = &cobra.Command{
 	Use:                "validate",
 	Short:              "Validate configurations against OPA policies and JSON schemas",
-	Long:               `This command validates stacks and components`,
+	Long:               `This command validates stacks and components by checking their configurations against Open Policy Agent (OPA) policies and JSON schemas.`,
 	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: false},
 }
 
