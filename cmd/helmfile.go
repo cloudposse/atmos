969d988bdbc50223c0eb85665b53fa397e1a2afd
Sam Tholiya <tholiya.sam@outlook.in>
2025-01-09 23:30:27 +0100

    rearrange the help strings

--- cmd/helmfile.go
+++ cmd/helmfile.go
@@ -6,42 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Command: atmos helmfile diff
-var helmfileDiffShort = "Show differences between the desired and actual state of Helm releases."
-var helmfileDiffLong = `This command calculates and displays the differences between the desired state of Helm releases
-defined in your configurations and the actual state deployed in the cluster.
-
-Example usage:
-  atmos helmfile diff echo-server -s tenant1-ue2-dev
-  atmos helmfile diff echo-server -s tenant1-ue2-dev --redirect-stderr /dev/null`
-
-// Command: atmos helmfile apply
-var helmfileApplyShort = "Apply changes to align the actual state of Helm releases with the desired state."
-var helmfileApplyLong = `This command reconciles the actual state of Helm releases in the cluster with the desired state
-defined in your configurations by applying the necessary changes.
-
-Example usage:
-  atmos helmfile apply echo-server -s tenant1-ue2-dev
-  atmos helmfile apply echo-server -s tenant1-ue2-dev --redirect-stderr /dev/stdout`
-
-// Command: atmos helmfile sync
-var helmfileSyncShort = "Synchronize the state of Helm releases with the desired state without making changes."
-var helmfileSyncLong = `This command ensures that the actual state of Helm releases in the cluster matches the desired
-state defined in your configurations without performing destructive actions.
-
-Example usage:
-  atmos helmfile sync echo-server --stack tenant1-ue2-dev
-  atmos helmfile sync echo-server --stack tenant1-ue2-dev --redirect-stderr ./errors.txt`
-
-// Command: atmos helmfile destroy
-var helmfileDestroyShort = "Destroy the Helm releases for the specified stack."
-var helmfileDestroyLong = `This command removes the specified Helm releases from the cluster, ensuring a clean state for
-the given stack.
-
-Example usage:
-  atmos helmfile destroy echo-server --stack=tenant1-ue2-dev
-  atmos helmfile destroy echo-server --stack=tenant1-ue2-dev --redirect-stderr /dev/stdout`
-
 // helmfileCmd represents the base command for all helmfile sub-commands
 var helmfileCmd = &cobra.Command{
 	Use:                "helmfile",
--- cmd/helmfile_diff.go
+++ cmd/helmfile_diff.go
@@ -4,6 +4,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Command: atmos helmfile diff
+var helmfileDiffShort = "Show differences between the desired and actual state of Helm releases."
+var helmfileDiffLong = `This command calculates and displays the differences between the desired state of Helm releases
+defined in your configurations and the actual state deployed in the cluster.
+
+Example usage:
+  atmos helmfile diff echo-server -s tenant1-ue2-dev
+  atmos helmfile diff echo-server -s tenant1-ue2-dev --redirect-stderr /dev/null`
+
 // helmfileDiffCmd represents the base command for all helmfile sub-commands
 var helmfileDiffCmd = &cobra.Command{
 	Use:                "diff",
