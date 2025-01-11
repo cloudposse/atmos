8d017ebd001c256208c5cdcee16bdd67f30c3130
Listener430 <95291462+Listener430@users.noreply.github.com>
2025-01-11 23:00:46 +0500

    Support `GITHUB_TOKEN` for HTTP Requests to github.com (#912)
    * token version
    
    * custom detector
    
    * config update
    
    * config/docs update
    
    * debug messages
    
    * Update website/docs/cli/configuration/configuration.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * ATMOS_GITHUB_TOKEN
    
    * Update website/docs/cli/configuration/configuration.mdx
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update internal/exec/vendor_utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update internal/exec/vendor_utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update internal/exec/vendor_utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * debug traces
    
    ---------
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- internal/exec/vendor_model.go
+++ internal/exec/vendor_model.go
@@ -245,7 +245,6 @@ func downloadAndInstall(p *pkgAtmosVendor, dryRun bool, atmosConfig schema.Atmos
 				name: p.name,
 			}
 		}
-
 		// Create temp directory
 		tempDir, err := os.MkdirTemp("", fmt.Sprintf("atmos-vendor-%d-*", time.Now().Unix()))
 		if err != nil {
@@ -269,6 +268,9 @@ func downloadAndInstall(p *pkgAtmosVendor, dryRun bool, atmosConfig schema.Atmos
 		switch p.pkgType {
 		case pkgTypeRemote:
 			// Use go-getter to download remote packages
+			// Register custom detectors
+			RegisterCustomDetectors(atmosConfig)
+
 			client := &getter.Client{
 				Ctx:  ctx,
 				Dst:  tempDir,
