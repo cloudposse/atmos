72dfe12ed2f377e656ff623051938db0c7935802
Sam Tholiya <tholiya.sam@outlook.in>
2025-01-15 21:51:45 +0100

    changed editorconfig fixture directory

---
+++ tests/fixtures/scenarios/editorconfig/components/terraform/test/template-functions-test2/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type        = string
+  description = "Region"
+}
+
+variable "test_label_id" {
+  type        = string
+  description = "Test label ID"
+}
+
+variable "test_label_id_2" {
+  type        = string
+  description = "Test label ID 2"
+}
