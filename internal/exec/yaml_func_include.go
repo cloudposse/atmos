04b628ab7199a0342b878d5437c4ab44e75e675d
aknysh <andriy.knysh@gmail.com>
2025-01-17 01:12:26 -0500

    updates

--- internal/exec/yaml_func_include.go
+++ internal/exec/yaml_func_include.go
@@ -1,8 +1,8 @@
 package exec
 
 import (
-	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/cloudposse/atmos/pkg/config"
 	"github.com/cloudposse/atmos/pkg/schema"
@@ -17,17 +17,39 @@ func processTagInclude(
 	u.LogTrace(atmosConfig, fmt.Sprintf("Executing Atmos YAML function: %s", input))
 
 	str, err := getStringAfterTag(input, config.AtmosYamlFuncInclude)
-
 	if err != nil {
 		u.LogErrorAndExit(atmosConfig, err)
 	}
 
-	res := str
+	// Split the value into slices based on any whitespace (one or more spaces, tabs, or newlines),
+	// while also ignoring leading and trailing whitespace
+	var f string
+	q := ""
+	parts := strings.Fields(str)
+	partsLen := len(parts)
 
-	var decoded any
-	if err = json.Unmarshal([]byte(res), &decoded); err != nil {
-		return res
+	if partsLen == 2 {
+		f = strings.TrimSpace(parts[0])
+		q = strings.TrimSpace(parts[1])
+	} else if partsLen == 1 {
+		f = strings.TrimSpace(parts[0])
+	} else {
+		err = fmt.Errorf("invalid number of arguments in the Atmos YAML function: '!include %s'. The function saccepts 1 or 2 arguments", f)
+		u.LogErrorAndExit(atmosConfig, err)
 	}
 
-	return decoded
+	var res any
+	err = u.DetectFormatAndParseFile(f, &res)
+	if err != nil {
+		u.LogErrorAndExit(atmosConfig, err)
+	}
+
+	if q != "" {
+		res, err = u.EvaluateYqExpression(&atmosConfig, res, q)
+		if err != nil {
+			u.LogErrorAndExit(atmosConfig, err)
+		}
+	}
+
+	return res
 }
