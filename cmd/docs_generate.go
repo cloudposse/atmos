f48f17ca8ac58baeda1d7769e8b1fd5f4f4a9aa9
Andrey Kalmykov <nkalmykova@gmail.com>
2025-01-22 18:50:10 +0600

    examples back

--- cmd/docs_generate.go
+++ cmd/docs_generate.go
@@ -6,32 +6,27 @@ import (
 	e "github.com/cloudposse/atmos/internal/exec"
 )
 
-// docsGenerateCmd genrates README.md
+// docsGenerateCmd generates README.md
 var docsGenerateCmd = &cobra.Command{
 	Use:     "generate [path]",
 	Aliases: []string{"docs"},
 	Short:   "Generate docs (README.md) from README.yaml data and templates",
-	Long: `Generate documentation by merging multiple README.yaml data sources 
-and then using templates to produce README.md files. Also supports terraform-docs injection.
+	Long: `Generate documentation by merging multiple README.yaml data sources
+and then using templates to produce README.md files. Also supports terraform-docs injection.`,
+	Example: `Generate a README.md in the current path:
+atmos docs generate
 
-Examples:
-  - Generate a README.md in the current path:
-      atmos docs generate
+Generate a README.md for the VPC component:
+atmos docs generate components/terraform/vpc
 
-  - Generate a README.md for the VPC component:
-      atmos docs generate components/terraform/vpc
-
-  - Generate all README.md (recursively searches for README.yaml to rebuild docs):
-      ```
-      atmos docs generate --all
-      ```
-`,
+Generate all README.md (recursively searches for README.yaml to rebuild docs):
+atmos docs generate --all`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return e.ExecuteDocsGenerateCmd(cmd, args)
 	},
 }
 
-// generateCmd is a new top-level command so we can do `atmos generate docs`.
+// generateDocsCmd is a new top-level command so we can do `atmos generate docs`.
 var generateDocsCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generic generation commands (e.g. docs, scaffolding, etc.)",
