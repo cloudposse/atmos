fbddec20de64ec74d92f23e72c5087327472e839
Sam Tholiya <tholiya.sam@outlook.in>
2025-02-01 00:11:16 +0100

    fix log level error

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -61,7 +61,7 @@ func ParseLogLevel(logLevel string) (LogLevel, error) {
 		}
 	}
 
-	return "", fmt.Errorf("Error: Invalid log level `%s`. Valid options are: %v", logLevel, validLevels)
+	return "", fmt.Errorf("Invalid log level `%s`. Valid options are: %v", logLevel, validLevels)
 }
 
 func (l *Logger) log(logColor *color.Color, message string) {
