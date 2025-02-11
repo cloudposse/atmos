83051df8dae3392f4a67a622bbbe860bccf093a4
haitham911 <haitham911eg@gmail.com>
2025-02-11 18:41:14 +0200

    refactor inferBasePath

--- pkg/config/base_path.go
+++ pkg/config/base_path.go
@@ -10,6 +10,7 @@ import (
 	u "github.com/cloudposse/atmos/pkg/utils"
 )
 
+// Derive the effective base-base based on multiple inferences
 func (cl *ConfigLoader) BasePathComputing(configAndStacksInfo schema.ConfigAndStacksInfo) (string, error) {
 	// Check base path from CLI argument
 	if configAndStacksInfo.BasePathFromArg != "" {
@@ -21,7 +22,6 @@ func (cl *ConfigLoader) BasePathComputing(configAndStacksInfo schema.ConfigAndSt
 		return cl.resolveAndValidatePath(envBasePath, "ATMOS_BASE_PATH")
 	}
 
-	// Check base path from configuration
 	// Check base path from configuration
 	if cl.atmosConfig.BasePath != "" {
 		source := "atmos config"
@@ -36,7 +36,7 @@ func (cl *ConfigLoader) BasePathComputing(configAndStacksInfo schema.ConfigAndSt
 		return cl.resolveAndValidatePath(cl.atmosConfig.BasePath, source)
 	}
 
-	resolvedPath, found := cl.infraBasePath()
+	resolvedPath, found := cl.inferBasePath()
 	if found {
 		absPath, err := filepath.Abs(resolvedPath)
 		if err != nil {
@@ -75,42 +75,61 @@ func (cl *ConfigLoader) resolveAndValidatePath(path string, source string) (stri
 	return absPath, nil
 }
 
-func (cl *ConfigLoader) infraBasePath() (string, bool) {
-	// CWD Check CWD for atmos.yaml, .atmos.yaml, atmos.d/**/*, .github/atmos.yaml
-	// if found Set base_path to absolute path containing directory
-	// Infer base_path
+// Check CWD for atmos.yaml, .atmos.yaml, atmos.d/**/*, .github/atmos.yaml
+// if found return base_path to absolute path containing directory
+func (cl *ConfigLoader) inferBasePath() (string, bool) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Debug("failed to get current working directory", "err", err)
 		return "", false
 	}
-	filePath, found := cl.SearchConfigFilePath(filepath.Join(cwd, "atmos"))
+
+	// Check for atmos.yaml or .atmos.yaml
+	if basePath, found := cl.checkConfigFileOrDir(filepath.Join(cwd, CliConfigFileName), "atmos configuration directory", false); found {
+		return basePath, true
+	}
+	// Check for .atmos directory
+	if basePath, found := cl.checkConfigFileOrDir(filepath.Join(cwd, ".atmos"), ".atmos configuration directory", false); found {
+		return basePath, true
+	}
+	found, _ := u.IsDirectory(filepath.ToSlash(filepath.Join(cwd, "atmos.d")))
 	if found {
-		log.Debug("base path derived from infra atmos file", "path", filePath)
-		return filepath.Dir(filePath), found
-	}
-	filePath, found = cl.SearchConfigFilePath(filepath.Join(cwd, ".atmos"))
-	if found {
-		log.Debug("base path derived from infra .atmos file", "path", filePath)
-		return filepath.Dir(filePath), found
-	}
-	filePaths, _ := u.GetGlobMatches(filepath.ToSlash(filepath.Join(cwd, "atmos.d/**/*.yaml")))
-	if len(filePaths) == 0 {
-		filePaths, _ = u.GetGlobMatches(filepath.ToSlash(filepath.Join(cwd, "atmos.d/**/*.yml")))
-	}
-	if len(filePaths) > 0 {
-		filePaths = cl.sortFilesByDepth(filePaths)
-		log.Debug("base path derived from infra atmos.d file", "path", filePaths[0])
-		return filepath.Dir(filePaths[0]), true
-	}
-	gitTopLevel, err := u.GetGitRoot()
-	if err == nil {
-		dirAbs, found := cl.SearchConfigFilePath(filepath.Join(gitTopLevel, "atmos"))
-		if found {
-			log.Debug("base path derived from infra git root", "path", filePath)
-			return dirAbs, found
+		// Check for atmos.d directory
+		if basePath, found := cl.checkConfigFileOrDir(filepath.Join(cwd, "atmos.d"), "atmos.d/ directory", true); found {
+			return basePath, true
 		}
 	}
 
+	// Check for git root
+	gitTopLevel, err := u.GetGitRoot()
+	if err == nil {
+		if basePath, found := cl.checkConfigFileOrDir(filepath.Join(gitTopLevel, CliConfigFileName), "git root", false); found {
+			return basePath, true
+		}
+	}
+
+	return "", false
+}
+
+// checkConfigFileOrDir checks for the existence of a file or directory and returns the base path if found.
+func (cl *ConfigLoader) checkConfigFileOrDir(path, desc string, isDir bool) (string, bool) {
+	if isDir {
+		filePaths, _ := u.GetGlobMatches(filepath.ToSlash(filepath.Join(path, "**/*.yaml")))
+		if len(filePaths) == 0 {
+			filePaths, _ = u.GetGlobMatches(filepath.ToSlash(filepath.Join(path, "**/*.yml")))
+		}
+		if len(filePaths) > 0 {
+			filePaths = cl.sortFilesByDepth(filePaths)
+			log.Debug("base path inferred from "+desc, "path", filepath.Dir(filePaths[0]))
+			return filepath.Dir(filePaths[0]), true
+		}
+
+	} else {
+		filePath, found := cl.SearchConfigFilePath(path)
+		if found {
+			log.Debug("base-path inferred from "+desc, "path", filepath.Dir(filePath))
+			return filepath.Dir(filePath), true
+		}
+	}
 	return "", false
 }
