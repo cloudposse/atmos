34d9a7306588a3c1633007c5b3df615a9f7fab00
Sam Tholiya <tholiya.sam@outlook.in>
2025-02-15 21:17:39 +0100

    add more test cases for exit codes

--- tests/fixtures/scenarios/exitCode/components/terraform/hook-and-store/main.tf
+++ tests/fixtures/scenarios/exitCode/components/terraform/hook-and-store/main.tf
@@ -1,15 +1,34 @@
-variable "stage" {
-  description = "Stage where it will be deployed"
-  type        = string
-}
-
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# Invalid resource block with a missing argument to cause terraform plan to fail
-resource "null_resource" "example" {
+variable "stage" {
+  description = "The file used as a flag to indicate that Terraform has already been applied."
+  type        = string
+  default     = "/tmp/terraform_once"
+}
+
+resource "null_resource" "run_once" {
+  # Use a changing trigger to force the provisioner to run every time.
+  triggers = {
+    always_run = timestamp()
+  }
+
   provisioner "local-exec" {
-    command = "echo ${var.stage}"
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<-EOT
+      if [ -f "${var.stage}" ]; then
+        echo "Flag file exists. Exiting with error as intended."
+        exit 1
+      else
+        echo "Flag file not found. Creating flag file."
+        touch "${var.stage}"
+      fi
+    EOT
   }
 }
