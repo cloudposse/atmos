2d17d733cfb17de0f66d22e94a13ed81f986753e
Sam Tholiya <tholiya.sam@outlook.in>
2025-02-17 20:48:55 +0100

    fixed <command> tag

--- cmd/root.go
+++ cmd/root.go
@@ -247,7 +247,7 @@ func init() {
 	cobra.AddTemplateFunc("wrappedFlagUsages", templates.WrappedFlagUsages)
 
 	RootCmd.PersistentFlags().String("redirect-stderr", "", "File descriptor to redirect 'stderr' to. "+
-		"Errors can be redirected to any file or any standard file descriptor (including '/dev/null'): atmos <command> --redirect-stderr /dev/stdout")
+		"Errors can be redirected to any file or any standard file descriptor (including '/dev/null'): atmos &lt;command&gt; --redirect-stderr /dev/stdout")
 
 	RootCmd.PersistentFlags().String("logs-level", "Info", "Logs level. Supported log levels are Trace, Debug, Info, Warning, Off. If the log level is set to Off, Atmos will not log any messages")
 	RootCmd.PersistentFlags().String("logs-file", "/dev/stderr", "The file to write Atmos logs to. Logs can be written to any file or any standard file descriptor, including '/dev/stdout', '/dev/stderr' and '/dev/null'")
