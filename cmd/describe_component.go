f712b53523733e6f32740e3591a9b42c3c6ea60d
Sam Tholiya <tholiya.sam@outlook.in>
2025-02-22 20:48:16 +0100

    updated help examples

--- cmd/describe_component.go
+++ cmd/describe_component.go
@@ -28,11 +28,11 @@ var describeComponentCmd = &cobra.Command{
 func init() {
 	describeComponentCmd.DisableFlagParsing = false
 	AddStackCompletion(describeComponentCmd)
-	describeComponentCmd.PersistentFlags().StringP("format", "f", "yaml", "The output format: atmos describe component &ltcomponent&gt -s &ltstack&gt --format=yaml|json (`yaml` is default)")
-	describeComponentCmd.PersistentFlags().String("file", "", "Write the result to the file: atmos describe component &ltcomponent&gt -s &ltstack&gt --file component.yaml")
-	describeComponentCmd.PersistentFlags().Bool("process-templates", true, "Enable/disable Go template processing in Atmos stack manifests when executing the command: atmos describe component &ltcomponent&gt -s &ltstack&gt --process-templates=false")
-	describeComponentCmd.PersistentFlags().Bool("process-functions", true, "Enable/disable YAML functions processing in Atmos stack manifests when executing the command: atmos describe component &ltcomponent&gt -s &ltstack&gt --process-functions=false")
-	describeComponentCmd.PersistentFlags().StringSlice("skip", nil, "Skip executing a YAML function in the Atmos stack manifests when executing the command: atmos describe component &ltcomponent&gt -s &ltstack&gt --skip=terraform.output")
+	describeComponentCmd.PersistentFlags().StringP("format", "f", "yaml", "The output format (`yaml` is default)")
+	describeComponentCmd.PersistentFlags().String("file", "", "Write the result to the file")
+	describeComponentCmd.PersistentFlags().Bool("process-templates", true, "Enable/disable Go template processing in Atmos stack manifests when executing the command")
+	describeComponentCmd.PersistentFlags().Bool("process-functions", true, "Enable/disable YAML functions processing in Atmos stack manifests when executing the command")
+	describeComponentCmd.PersistentFlags().StringSlice("skip", nil, "Skip executing a YAML function in the Atmos stack manifests when executing the command")
 
 	err := describeComponentCmd.MarkPersistentFlagRequired("stack")
 	if err != nil {
