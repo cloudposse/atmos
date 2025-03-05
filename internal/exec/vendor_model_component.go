5075c55c43a1a94a9830d084c198a724050f6c2d
Andrey Kalmykov <nkalmykova@gmail.com>
2025-03-06 02:39:58 +0500

    more linter fixes

--- internal/exec/vendor_model_component.go
+++ internal/exec/vendor_model_component.go
@@ -128,7 +128,7 @@ func installComponent(p *pkgComponentVendor, atmosConfig schema.AtmosConfigurati
 		tempDir = filepath.Join(tempDir, SanitizeFileName(p.uri))
 
 		if err = GoGetterGet(&atmosConfig, p.uri, tempDir, getter.ClientModeAny, 10*time.Minute); err != nil {
-			return fmt.Errorf("failed to download package %s: %w", p.name, err)
+			return fmt.Errorf("failed to download package %s: %w", p.name, err) //nolint:err113
 		}
 
 	case pkgTypeOci:
@@ -158,7 +158,7 @@ func installComponent(p *pkgComponentVendor, atmosConfig schema.AtmosConfigurati
 			return fmt.Errorf("failed to copy package %s: %w", p.name, err)
 		}
 	default:
-		return fmt.Errorf("unknown package type %s package %s", p.pkgType.String(), p.name)
+		return fmt.Errorf("unknown package type %s package %s", p.pkgType.String(), p.name) //nolint:err113
 
 	}
 	if err = copyComponentToDestination(atmosConfig, tempDir, p.componentPath, p.vendorComponentSpec, p.sourceIsLocalFile, p.uri); err != nil {
