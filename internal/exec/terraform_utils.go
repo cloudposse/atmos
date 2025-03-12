4719d0db42e5daa5559ffc991bea9ca74f499f11
Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
2025-03-11 22:28:35 -0500

    Update internal/exec/terraform_utils.go

--- internal/exec/terraform_utils.go
+++ internal/exec/terraform_utils.go
@@ -154,7 +154,7 @@ func isWorkspacesEnabled(atmosConfig *schema.AtmosConfiguration, info *schema.Co
 	if info.ComponentBackendType == "http" {
 		// If workspaces are explicitly enabled for HTTP backend, log a warning.
 		if atmosConfig.Components.Terraform.WorkspacesEnabled != nil && *atmosConfig.Components.Terraform.WorkspacesEnabled {
-			l.Warn("Ignoring unsupported `enabled` setting for workspaces HTTP backend type.",
+			l.Warn("ignoring unsupported workspaces `enabled` setting for HTTP backend type.",
 				"backend", "http",
 				"component", info.Component)
 		}
