538f735167f9fc0d54bf627f911788e69156fe73
Cerebrovinny <vinny.cardoso96@gmail.com>
2025-03-17 15:10:52 +0000

    Improve error message formatting; use wrapped errors

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -142,7 +142,7 @@ func ParseLogLevel(logLevel string) (log.Level, error) {
 	}
 
 	validLevels := []string{"Trace", "Debug", "Info", "Warning", "Off"}
-	return 0, fmt.Errorf("Invalid log level `%s`. Valid options are: %v", logLevel, validLevels)
+	return 0, fmt.Errorf("%w: %s (valid options are: %v)", ErrInvalidLogLevel, logLevel, validLevels)
 }
 
 func (l *Logger) SetLogLevel(logLevel log.Level) error {
