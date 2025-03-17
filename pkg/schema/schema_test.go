f2cff9670b630f5f988e6f8969593e5ee050c39f
Sam Tholiya <tholiya.sam@outlook.in>
2025-03-17 03:20:09 +0100

    Update atmos schema (#1123)
    * Update atmos schema init
    
    * updated schema
    
    * refactor schema name
    
    * fixed test case
    
    * processSchemas updated
    
    * fix golangci lint
    
    * fix the downloadSchemaFromURL lint
    
    * lint fix validate stacks
    
    * lint fix
    
    * fixed lints
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fixed the processCommandLineArgs lint
    
    * fix lint
    
    * fix lint
    
    * Update internal/exec/validate_stacks.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * Update pkg/config/utils.go
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    
    * fix tests
    
    * fixed log messages
    
    ---------
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

---
+++ pkg/schema/schema_test.go
@@ -0,0 +1,38 @@
+package schema
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+func TestAtmosConfigurationWorksWithOpa(t *testing.T) {
+	yamlString := `
+schemas:
+  opa:
+    base_path: "some/random/path"
+`
+	atmosConfig := &AtmosConfiguration{}
+	err := yaml.Unmarshal([]byte(yamlString), atmosConfig)
+	assert.NoError(t, err)
+	resourcePath := atmosConfig.GetResourcePath("opa")
+	assert.Equal(t, "some/random/path", resourcePath.BasePath)
+}
+
+func TestAtmosConfigurationWithSchemas(t *testing.T) {
+	yamlString := `
+schemas:
+  atmos:
+    manifest: "some/random/path"
+    matches:
+      - hello
+      - world
+`
+	atmosConfig := &AtmosConfiguration{}
+	err := yaml.Unmarshal([]byte(yamlString), atmosConfig)
+	assert.NoError(t, err)
+	schemas := atmosConfig.GetSchemaRegistry("atmos")
+	assert.Equal(t, "some/random/path", schemas.Manifest)
+	assert.Equal(t, []string{"hello", "world"}, schemas.Matches)
+}
