ae71bc154d3194ecc292c65a2c2b0a6f4909f5a6
Andriy Knysh <aknysh@users.noreply.github.com>
2025-03-23 14:52:02 -0400

    Apply suggestions from code review
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- internal/exec/template_funcs_gomplate_datasource.go
+++ internal/exec/template_funcs_gomplate_datasource.go
@@ -11,7 +11,7 @@ import (
 var gomplateDatasourceFuncSyncMap = sync.Map{}
 
 func gomplateDatasourceFunc(alias string, gomplateData *data.Data, args ...string) (any, error) {
-	log.Debug(fmt.Sprintf("atmos.GomplateDatasource(): processing datasource alias '%s'", alias))
+  log.Debug("atmos.GomplateDatasource(): processing datasource", "alias", alias)
 
 	// If the result for the alias already exists in the cache, return it
 	existingResult, found := gomplateDatasourceFuncSyncMap.Load(alias)
@@ -27,7 +27,7 @@ func gomplateDatasourceFunc(alias string, gomplateData *data.Data, args ...strin
 	// Cache the result
 	gomplateDatasourceFuncSyncMap.Store(alias, result)
 
-	log.Debug(fmt.Sprintf("atmos.GomplateDatasource(): processed datasource alias '%s'.\nResult: '%v'", alias, result))
+	log.Debug("atmos.GomplateDatasource(): processed datasource", "alias",  alias, "result", result)
 
 	return result, nil
 }
