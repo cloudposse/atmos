aabcb31818c31b7a7696c345e23fe914d1fc276b
Sam Tholiya <tholiya.sam@outlook.in>
2025-04-05 21:33:07 +0200

    logging

--- tests/terraform_test.go
+++ tests/terraform_test.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/charmbracelet/log"
+	log "github.com/charmbracelet/log"
 	"github.com/cloudposse/atmos/cmd"
 	"github.com/cloudposse/atmos/pkg/utils"
 	"github.com/stretchr/testify/assert"
