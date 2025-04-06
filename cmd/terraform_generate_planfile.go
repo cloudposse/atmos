7ddb89b304eea349eeebd30d33b82d2f7420d952
aknysh <andriy.knysh@gmail.com>
2025-04-06 17:32:59 -0400

    updates

---
+++ cmd/terraform_generate_planfile.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	e "github.com/cloudposse/atmos/internal/exec"
+	u "github.com/cloudposse/atmos/pkg/utils"
+)
+
+// terraformGeneratePlanfileCmd generates planfile for a terraform component
+var terraformGeneratePlanfileCmd = &cobra.Command{
+	Use:                "planfile",
+	Short:              "Generate a planfile for a Terraform component",
+	Long:               "This command generates a `planfile` for a specified Atmos Terraform component.",
+	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: false},
+	ValidArgsFunction:  ComponentsArgCompletion,
+	Run: func(cmd *cobra.Command, args []string) {
+		handleHelpRequest(cmd, args)
+		// Check Atmos configuration
+		checkAtmosConfig()
+
+		err := e.ExecuteTerraformGenerateVarfileCmd(cmd, args)
+		if err != nil {
+			u.PrintErrorMarkdownAndExit("", err, "")
+		}
+	},
+}
+
+func init() {
+	terraformGeneratePlanfileCmd.DisableFlagParsing = false
+	AddStackCompletion(terraformGeneratePlanfileCmd)
+	terraformGeneratePlanfileCmd.PersistentFlags().StringP("file", "f", "", "Path to the planfile to generate for the specified Terraform component in the given stack.")
+
+	err := terraformGeneratePlanfileCmd.MarkPersistentFlagRequired("stack")
+	if err != nil {
+		u.PrintErrorMarkdownAndExit("", err, "")
+	}
+
+	terraformGenerateCmd.AddCommand(terraformGeneratePlanfileCmd)
+}
