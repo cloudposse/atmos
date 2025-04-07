8d4ee364fcd3444bcb1c1e3e2c8f8bb322a9fb81
Sam Tholiya <tholiya.sam@outlook.in>
2025-04-07 23:35:56 +0200

    fixed config test

--- tests/config_test.go
+++ tests/config_test.go
@@ -39,3 +39,36 @@ func TestFindAllStackConfigsInPathsForStack(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "deploy/nonprod.yaml", relativePaths[0])
 }
+
+func TestFindAllStackConfigsInPaths(t *testing.T) {
+	stacksPath := "./fixtures/scenarios/atmos-overrides-section"
+
+	err := os.Setenv("ATMOS_CLI_CONFIG_PATH", stacksPath)
+	assert.NoError(t, err, "Setting 'ATMOS_CLI_CONFIG_PATH' environment variable should execute without error")
+
+	err = os.Setenv("ATMOS_BASE_PATH", stacksPath)
+	assert.NoError(t, err, "Setting 'ATMOS_BASE_PATH' environment variable should execute without error")
+
+	// Unset env values after testing
+	defer func() {
+		err := os.Unsetenv("ATMOS_BASE_PATH")
+		assert.NoError(t, err)
+		err = os.Unsetenv("ATMOS_CLI_CONFIG_PATH")
+		assert.NoError(t, err)
+	}()
+
+	configAndStacksInfo := schema.ConfigAndStacksInfo{}
+	atmosConfig, err := config.InitCliConfig(configAndStacksInfo, true)
+	assert.NoError(t, err)
+
+	_, relativePaths, err := config.FindAllStackConfigsInPaths(
+		atmosConfig,
+		atmosConfig.IncludeStackAbsolutePaths,
+		atmosConfig.ExcludeStackAbsolutePaths,
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, "deploy/dev.yaml", relativePaths[0])
+	assert.Equal(t, "deploy/prod.yaml", relativePaths[1])
+	assert.Equal(t, "deploy/sandbox.yaml", relativePaths[2])
+	assert.Equal(t, "deploy/staging.yaml", relativePaths[3])
+}
