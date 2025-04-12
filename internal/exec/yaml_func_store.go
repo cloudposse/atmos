9b4b1b8e92fbf59717367fc4be12a6af18f6d06c
aknysh <andriy.knysh@gmail.com>
2025-04-11 23:30:08 -0400

    updates

--- internal/exec/yaml_func_store.go
+++ internal/exec/yaml_func_store.go
@@ -1,6 +1,7 @@
 package exec
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -19,8 +20,12 @@ type params struct {
 	defaultValue *string
 }
 
+var (
+	ErrInvalidYamlFuncStore = errors.New("invalid YAML function")
+)
+
 func processTagStore(atmosConfig schema.AtmosConfiguration, input string, currentStack string) any {
-	log.Debug("Executing Atmos YAML function store", "input", input)
+	log.Debug("Executing Atmos YAML function", "function", input)
 
 	str, err := getStringAfterTag(input, u.AtmosYamlFuncStore)
 	if err != nil {
@@ -32,35 +37,47 @@ func processTagStore(atmosConfig schema.AtmosConfiguration, input string, curren
 	storePart := strings.TrimSpace(parts[0])
 
 	var defaultValue *string
+	var query string
 	if len(parts) > 1 {
-		// Expecting the format: default <value>
-		defaultParts := strings.Fields(strings.TrimSpace(parts[1]))
-		if len(defaultParts) != 2 || defaultParts[0] != "default" {
-			log.Error(fmt.Sprintf("invalid default value format in: %s", str))
-			return fmt.Sprintf("invalid default value format in: %s", str)
+		// Expecting the format: default <value> or query <yq-expression>
+		for _, p := range parts[1:] {
+			pipeParts := strings.Fields(strings.TrimSpace(p))
+			if len(pipeParts) != 2 {
+				e := fmt.Errorf("%w: %s", ErrInvalidYamlFuncStore, input)
+				log.Error(e)
+				return fmt.Sprintf("invalid YAML function: %s", input)
+			}
+			v1 := strings.Trim(pipeParts[0], `"'`) // Remove surrounding quotes if present
+			v2 := strings.Trim(pipeParts[1], `"'`) // Remove surrounding quotes if present
+			switch v1 {
+			case "default":
+				defaultValue = &v2
+			case "query":
+				query = v2
+			default:
+				e := fmt.Errorf("%w: %s", ErrInvalidYamlFuncStore, input)
+				log.Error(e)
+				return fmt.Sprintf("invalid YAML function: %s", input)
+			}
 		}
-		val := strings.Trim(defaultParts[1], `"'`) // Remove surrounding quotes if present
-		defaultValue = &val
 	}
 
 	// Process the main store part
 	storeParts := strings.Fields(storePart)
 	partsLength := len(storeParts)
-	if partsLength != 3 && partsLength != 4 && partsLength != 5 {
-		return fmt.Sprintf("invalid Atmos Store YAML function execution:: %s\ninvalid parameters: store_name, {stack}, component, key, {yq-expression}", input)
+	if partsLength != 3 && partsLength != 4 {
+		e := fmt.Errorf("%w: %s", ErrInvalidYamlFuncStore, input)
+		log.Error(e)
+		return fmt.Sprintf("invalid YAML function: %s", input)
 	}
 
 	retParams := params{
 		storeName:    strings.TrimSpace(storeParts[0]),
 		defaultValue: defaultValue,
+		query:        query,
 	}
 
-	if partsLength == 5 {
-		retParams.stack = strings.TrimSpace(storeParts[1])
-		retParams.component = strings.TrimSpace(storeParts[2])
-		retParams.key = strings.TrimSpace(storeParts[3])
-		retParams.query = strings.TrimSpace(storeParts[4])
-	} else if partsLength == 4 {
+	if partsLength == 4 {
 		retParams.stack = strings.TrimSpace(storeParts[1])
 		retParams.component = strings.TrimSpace(storeParts[2])
 		retParams.key = strings.TrimSpace(storeParts[3])
@@ -74,7 +91,7 @@ func processTagStore(atmosConfig schema.AtmosConfiguration, input string, curren
 	store := atmosConfig.Stores[retParams.storeName]
 
 	if store == nil {
-		log.Fatal(fmt.Errorf("invalid Atmos Store YAML function execution:: %s\nstore '%s' not found", input, retParams.storeName))
+		log.Fatal(fmt.Errorf("%w: %s\nstore '%s' not found", ErrInvalidYamlFuncStore, input, retParams.storeName))
 	}
 
 	// Retrieve the value from the store
@@ -83,7 +100,7 @@ func processTagStore(atmosConfig schema.AtmosConfiguration, input string, curren
 		if retParams.defaultValue != nil {
 			return *retParams.defaultValue
 		}
-		log.Fatal(fmt.Errorf("failed to get key: %s", err))
+		log.Fatal(fmt.Errorf("%w: %s\nfailed to get key: %s\nerror: %v", ErrInvalidYamlFuncStore, input, retParams.key, err))
 	}
 
 	// Execute the YQ expression
@@ -92,7 +109,7 @@ func processTagStore(atmosConfig schema.AtmosConfiguration, input string, curren
 	if retParams.query != "" {
 		res, err = u.EvaluateYqExpression(&atmosConfig, value, retParams.query)
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
 	} else {
 		res = value
