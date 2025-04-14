222938e81f54060fb1278be8310060f8e9225197
Sam Tholiya <tholiya.sam@outlook.in>
2025-04-15 00:48:03 +0200

    added more test for utils

--- tests/config_test.go
+++ tests/config_test.go
@@ -2,8 +2,10 @@ package tests
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/cloudposse/atmos/internal/exec"
 	"github.com/cloudposse/atmos/pkg/config"
 	"github.com/cloudposse/atmos/pkg/schema"
 	"github.com/stretchr/testify/assert"
@@ -71,4 +73,42 @@ func TestFindAllStackConfigsInPaths(t *testing.T) {
 	assert.Equal(t, "deploy/prod.yaml", relativePaths[1])
 	assert.Equal(t, "deploy/sandbox.yaml", relativePaths[2])
 	assert.Equal(t, "deploy/staging.yaml", relativePaths[3])
+}
+
+func TestValidateStacks(t *testing.T) {
+	basePath := "../../tests/fixtures/scenarios/atmos-overrides-section/stacks"
+
+	basePathAbs, err := filepath.Abs(basePath)
+	assert.Nil(t, err)
+
+	stackConfigFilesAbsolutePaths := []string{
+		filepath.Join(basePathAbs, "deploy", "dev.yaml"),
+		filepath.Join(basePathAbs, "deploy", "prod.yaml"),
+		filepath.Join(basePathAbs, "deploy", "sandbox.yaml"),
+		filepath.Join(basePathAbs, "deploy", "staging.yaml"),
+		filepath.Join(basePathAbs, "deploy", "test.yaml"),
+	}
+
+	atmosConfig := schema.AtmosConfiguration{
+		BasePath:                      basePath,
+		StacksBaseAbsolutePath:        basePathAbs,
+		StackConfigFilesAbsolutePaths: stackConfigFilesAbsolutePaths,
+		Stacks: schema.Stacks{
+			NamePattern: "{stage}",
+		},
+		Templates: schema.Templates{
+			Settings: schema.TemplatesSettings{
+				Enabled: true,
+				Sprig: schema.TemplatesSettingsSprig{
+					Enabled: true,
+				},
+				Gomplate: schema.TemplatesSettingsGomplate{
+					Enabled: true,
+				},
+			},
+		},
+	}
+
+	err = exec.ValidateStacks(atmosConfig)
+	assert.Nil(t, err)
 }
