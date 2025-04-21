4c704ae90430f991e5da344a95bc18cdc97b1311
aknysh <andriy.knysh@gmail.com>
2025-04-21 12:50:31 -0400

    updates

--- internal/exec/terraform_generate_planfile.go
+++ internal/exec/terraform_generate_planfile.go
@@ -1,12 +1,14 @@
 package exec
 
 import (
-	"errors"
+	u "github.com/cloudposse/atmos/pkg/utils"
+	"os"
+
 	log "github.com/charmbracelet/log"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	cfg "github.com/cloudposse/atmos/pkg/config"
-	u "github.com/cloudposse/atmos/pkg/utils"
 )
 
 // ExecuteTerraformGeneratePlanfileCmd executes `terraform generate planfile` command.
@@ -66,15 +68,45 @@ func ExecuteTerraformGeneratePlanfileCmd(cmd *cobra.Command, args []string) erro
 		planFileNameFromArg = ""
 	}
 
-	if len(planFileNameFromArg) > 0 {
+	componentWorkingDir := constructTerraformComponentWorkingDir(atmosConfig, info)
+
+	if planFileNameFromArg != "" {
 		planFilePath = planFileNameFromArg
 	} else {
-		planFilePath = constructTerraformComponentPlanfilePath(atmosConfig, info)
+		planFilePath = componentWorkingDir
+	}
+
+	// Create a temporary directory for all temporary files
+	tmpDir, err := os.MkdirTemp("", "atmos-terraform-generate-planfile")
+	if err != nil {
+		return errors.Wrap(err, "error creating temporary directory")
+	}
+	defer func(path string) {
+		err := os.RemoveAll(path)
+		if err != nil {
+			log.Warn("Error removing temporary directory", "path", path, "error", err)
+		}
+	}(tmpDir)
+
+	opts := PlanFileOptions{
+		ComponentPath: componentWorkingDir,
+		TmpDir:        tmpDir,
+	}
+
+	planFile, err := prepareNewPlanFile(&atmosConfig, &info, opts)
+	if err != nil {
+		return err
+	}
+
+	// Get the JSON representation of the new plan
+	planJSON, err := getTerraformPlanJSON(&atmosConfig, &info, componentWorkingDir, planFile)
+	if err != nil {
+		return errors.Wrap(err, "error getting JSON for planfile")
 	}
 
 	log.Debug("Writing the planfile", "file", planFilePath)
 
-	err = u.WriteToFileAsJSON(planFilePath, info.ComponentVarsSection, 0o644)
+	err = u.WriteToFileAsJSON(componentWorkingDir+".planfile.json", planJSON, 0o644)
 	if err != nil {
 		return err
 	}
