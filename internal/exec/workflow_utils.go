96936f3ff856dff3644d131f998810145250978b
Vinicius C. <vinny.cardoso96@gmail.com>
2025-05-11 12:31:53 +0100

    Update internal/exec/workflow_utils.go
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- internal/exec/workflow_utils.go
+++ internal/exec/workflow_utils.go
@@ -40,7 +40,7 @@ func ExecuteWorkflow(
 	// Check if the workflow steps have the `name` attribute
 	checkAndGenerateWorkflowStepNames(workflowDefinition)
 
-	logFunc(fmt.Sprintf("\nExecuting the workflow '%s' from '%s'\n", workflow, workflowPath))
+	log.Debug("Executing the workflow", "workflow", workflow, "file", workflowPath)
 
 	if atmosConfig.Logs.Level == u.LogLevelTrace || atmosConfig.Logs.Level == u.LogLevelDebug {
 		err := u.PrintAsYAMLToFileDescriptor(&atmosConfig, workflowDefinition)
