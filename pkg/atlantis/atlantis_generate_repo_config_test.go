2143c723c37be43b35c7373711d391c768c1e5d5
milldr <miller0daniel@gmail.com>
2025-06-06 18:27:47 -0400

    Update test case to remove unnecessary variable usage

--- pkg/atlantis/atlantis_generate_repo_config_test.go
+++ pkg/atlantis/atlantis_generate_repo_config_test.go
@@ -82,19 +82,15 @@ func TestExecuteAtlantisGenerateRepoConfigAffectedOnly(t *testing.T) {
 	atmosConfig, err := cfg.InitCliConfig(schema.ConfigAndStacksInfo{}, true)
 	assert.Nil(t, err)
 
-	// We are using `atmos.yaml` from this dir. This `atmos.yaml` has set base_path: "./",
+	// We are using `atmos.yaml` from this dir. This `atmos.yaml` has set base_path: "../../tests/fixtures/scenarios/complete",
 	// which will be wrong for the remote repo which is cloned into a temp dir.
 	// Set the correct base path for the cloned remote repo
 	atmosConfig.BasePath = "./tests/fixtures/scenarios/complete"
 
-	// Point to the same local repository
-	// This will compare this local repository with itself as the remote target, which should result in an empty `affected` list
-	repoPath := "../../"
-
 	err = e.ExecuteAtlantisGenerateRepoConfigAffectedOnly(
 		atmosConfig,
 		"/dev/stdout",
-		repoPath,
+		"",
 		"",
 		"",
 		"",
