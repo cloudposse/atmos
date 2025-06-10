845d50f34e3cbbe894cf76ce51f11e485d8ab703
milldr <miller0daniel@gmail.com>
2025-06-10 14:11:27 -0400

    merged

--- pkg/pro/api_client_drift_detection.go
+++ pkg/pro/api_client_drift_detection.go
@@ -11,7 +11,9 @@ import (
 	"github.com/cloudposse/atmos/pkg/utils"
 )
 
-var ErrFailedToUploadDriftDetection = errors.New("failed to upload drift detection results")
+var (
+	ErrFailedToUploadDriftDetection = errors.New("failed to upload drift detection results")
+)
 
 // UploadDriftDetection uploads drift detection data to the API.
 func (c *AtmosProAPIClient) UploadDriftDetection(dto *dtos.DriftDetectionUploadRequest) error {
