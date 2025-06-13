096072d2f7f6ebeb949e2eb4de5528782f8f4ff6
Sam Tholiya <tholiya.sam@outlook.in>
2025-06-13 21:55:23 +0200

    add pager to describe workflow (#1266)
    * add pager to describe config command
    
    * add model test
    
    * [autofix.ci] apply automated fixes
    
    * update structure
    
    * update with unit test cases
    
    * [autofix.ci] apply automated fixes
    
    * remove unwanted interface
    
    * fix tests
    
    * updated describe config
    
    * implement status with help
    
    * [autofix.ci] apply automated fixes
    
    * error view added
    
    * Update pkg/pager/model.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix tests
    
    * fix lint
    
    * fix golangci-lint
    
    * updated pager variable
    
    * fix tests
    
    * fixed test cases
    
    * update the pager variable logic
    
    * updated the pager logic
    
    * updated test case
    
    * fix pager bool
    
    * add pager for describe component
    
    * fix test case
    
    * fix test cases
    
    * Update the docs
    
    * add pager to describe config command
    
    * add model test
    
    * [autofix.ci] apply automated fixes
    
    * update structure
    
    * update with unit test cases
    
    * remove unwanted interface
    
    * [autofix.ci] apply automated fixes
    
    * fix tests
    
    * updated describe config
    
    * implement status with help
    
    * error view added
    
    * [autofix.ci] apply automated fixes
    
    * fix tests
    
    * Update pkg/pager/model.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix lint
    
    * fix golangci-lint
    
    * updated pager variable
    
    * fix tests
    
    * fixed test cases
    
    * update the pager variable logic
    
    * updated the pager logic
    
    * updated test case
    
    * fix pager bool
    
    * unit test case for isPagerEnabled
    
    * fix error name
    
    * fix tests
    
    * checking error in runner
    
    * fix struct
    
    * fix tests
    
    * add tests
    
    * added tests
    
    * added test for cmd
    
    * added tests for print as json
    
    * revert changes to limit the pr contents
    
    * shift common code
    
    * fix command
    
    * fix test coverage scope
    
    * add more test cases
    
    * [autofix.ci] apply automated fixes
    
    * add more test cases
    
    * describe affected with pager
    
    * [autofix.ci] apply automated fixes
    
    * fix complexity
    
    * updated scroll function
    
    * just checking
    
    * push highlighted utils golint fix
    
    * make it more unit testable
    
    * [autofix.ci] apply automated fixes
    
    * fix lint
    
    * [autofix.ci] apply automated fixes
    
    * fix golint
    
    * [autofix.ci] apply automated fixes
    
    * fix build issue
    
    * remove unwanted function
    
    * fix code rabbit
    
    * fix code rabbit
    
    * fix flag
    
    * fix default format
    
    * add test cases
    
    * [autofix.ci] apply automated fixes
    
    * fix test
    
    * fix lint
    
    * fix
    
    * test updated
    
    * fix test case
    
    * add more test coverage
    
    * init changes describe stacks for pager
    
    * fix comments
    
    * describe stacks with scroll
    
    * add test case
    
    * [autofix.ci] apply automated fixes
    
    * [autofix.ci] apply automated fixes
    
    * add tests
    
    * add pager to describe workflow
    
    * add tests
    
    * add tests
    
    * fix lint
    
    * [autofix.ci] apply automated fixes
    
    * fix coderabbit
    
    * fix pager flag
    
    * Update cmd/describe_affected.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix test
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
    Co-authored-by: Benjamin Smith <ben.smith.developer@gmail.com>

--- cmd/describe_workflows.go
+++ cmd/describe_workflows.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
+	"fmt"
 
-	e "github.com/cloudposse/atmos/internal/exec"
-	u "github.com/cloudposse/atmos/pkg/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+
+	"github.com/cloudposse/atmos/internal/exec"
+	cfg "github.com/cloudposse/atmos/pkg/config"
+	"github.com/cloudposse/atmos/pkg/schema"
 )
 
 // describeWorkflowsCmd executes 'atmos describe workflows' CLI commands
@@ -14,12 +18,31 @@ var describeWorkflowsCmd = &cobra.Command{
 	Long:               "List all Atmos workflows, showing their associated files and workflow names for easy reference.",
 	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: false},
 	Args:               cobra.NoArgs,
-	Run: func(cmd *cobra.Command, args []string) {
-		err := e.ExecuteDescribeWorkflowsCmd(cmd, args)
-		if err != nil {
-			u.PrintErrorMarkdownAndExit("", err, "")
-		}
-	},
+	Run:                getRunnableDescribeWorkflowsCmd(checkAtmosConfig, exec.ProcessCommandLineArgs, cfg.InitCliConfig, exec.NewDescribeWorkflowsExec()),
+}
+
+func getRunnableDescribeWorkflowsCmd(
+	checkAtmosConfig func(opts ...AtmosValidateOption),
+	processCommandLineArgs func(componentType string, cmd *cobra.Command, args []string, additionalArgsAndFlags []string) (schema.ConfigAndStacksInfo, error),
+	initCliConfig func(info schema.ConfigAndStacksInfo, validate bool) (schema.AtmosConfiguration, error),
+	describeWorkflowsExec exec.DescribeWorkflowsExec,
+) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		// Check Atmos configuration
+		checkAtmosConfig()
+		info, err := processCommandLineArgs("terraform", cmd, args, nil)
+		checkErrorAndExit(err)
+		atmosConfig, err := initCliConfig(info, true)
+		checkErrorAndExit(err)
+		describeWorkflowArgs := &exec.DescribeWorkflowsArgs{}
+		err = flagsToDescribeWorkflowsArgs(cmd.Flags(), describeWorkflowArgs)
+		checkErrorAndExit(err)
+		pager, err := cmd.Flags().GetString("pager")
+		checkFlagNotPresentError(err)
+		atmosConfig.Settings.Terminal.Pager = pager
+		err = describeWorkflowsExec.Execute(&atmosConfig, describeWorkflowArgs)
+		checkErrorAndExit(err)
+	}
 }
 
 func init() {
@@ -27,4 +50,54 @@ func init() {
 	describeWorkflowsCmd.PersistentFlags().StringP("output", "o", "list", "Specify the output type (`list` is default)")
 
 	describeCmd.AddCommand(describeWorkflowsCmd)
+}
+
+func flagsToDescribeWorkflowsArgs(flags *pflag.FlagSet, describe *exec.DescribeWorkflowsArgs) error {
+	if err := setStringFlagIfChanged(flags, "format", &describe.Format); err != nil {
+		return err
+	}
+	if err := setStringFlagIfChanged(flags, "output", &describe.OutputType); err != nil {
+		return err
+	}
+	if err := setStringFlagIfChanged(flags, "query", &describe.Query); err != nil {
+		return err
+	}
+
+	if err := validateAndSetDefaults(describe); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setStringFlagIfChanged(flags *pflag.FlagSet, name string, target *string) error {
+	if flags.Changed(name) {
+		val, err := flags.GetString(name)
+		if err != nil {
+			return err
+		}
+		*target = val
+	}
+	return nil
+}
+
+var (
+	ErrInvalidOutputType = fmt.Errorf("invalid output type specified. Valid values are 'list', 'map', and 'all'")
+	ErrInvalidFormat     = fmt.Errorf("invalid format specified. Valid values are 'yaml' and 'json'")
+)
+
+func validateAndSetDefaults(describe *exec.DescribeWorkflowsArgs) error {
+	if describe.Format == "" {
+		describe.Format = "yaml"
+	} else if describe.Format != "yaml" && describe.Format != "json" {
+		return ErrInvalidFormat
+	}
+
+	if describe.OutputType == "" {
+		describe.OutputType = "list"
+	} else if describe.OutputType != "list" && describe.OutputType != "map" && describe.OutputType != "all" {
+		return ErrInvalidOutputType
+	}
+
+	return nil
 }
