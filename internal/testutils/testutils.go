127955c9a0b0b53814e93284cb7e6e486bd187ce
Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>
2025-06-23 17:13:52 -0500

    Clarify purpose of testutils stub

--- internal/testutils/testutils.go
+++ internal/testutils/testutils.go
@@ -1,3 +1,7 @@
 package testutils
 
 // Package testutils provides helpers used only in tests.
+//
+// This stub file keeps the package buildable so that other
+// packages can import "internal/testutils" even though all
+// actual code lives in *_test.go files.
