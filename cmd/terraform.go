61763fbf59c4d41cb6552b5758770bb9a13897a8
aknysh <andriy.knysh@gmail.com>
2025-06-23 22:06:39 -0400

    check TF flags, add tests

--- cmd/terraform.go
+++ cmd/terraform.go
@@ -1,17 +1,7 @@
 package cmd
 
 import (
-	"errors"
-	"fmt"
-
-	log "github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
-
-	e "github.com/cloudposse/atmos/internal/exec"
-	cfg "github.com/cloudposse/atmos/pkg/config"
-	terrerrors "github.com/cloudposse/atmos/pkg/errors"
-	h "github.com/cloudposse/atmos/pkg/hooks"
-	u "github.com/cloudposse/atmos/pkg/utils"
 )
 
 // terraformCmd represents the base command for all terraform sub-commands
@@ -23,8 +13,6 @@ var terraformCmd = &cobra.Command{
 	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: true},
 }
 
-var ErrInvalidTerraformFlags = errors.New("the `--affected` flag can't be used with the `component` argument and with the `--all`, `--query` or `--components` flags")
-
 func init() {
 	// https://github.com/spf13/cobra/issues/739
 	terraformCmd.DisableFlagParsing = true
@@ -32,131 +20,4 @@ func init() {
 	AddStackCompletion(terraformCmd)
 	attachTerraformCommands(terraformCmd)
 	RootCmd.AddCommand(terraformCmd)
-}
-
-func runHooks(event h.HookEvent, cmd *cobra.Command, args []string) error {
-	info := getConfigAndStacksInfo("terraform", cmd, append([]string{cmd.Name()}, args...))
-
-	// Initialize the CLI config
-	atmosConfig, err := cfg.InitCliConfig(info, true)
-	if err != nil {
-		return fmt.Errorf("error initializing CLI config: %w", err)
-	}
-
-	hooks, err := h.GetHooks(&atmosConfig, &info)
-	if err != nil {
-		return fmt.Errorf("error getting hooks: %w", err)
-	}
-
-	if hooks != nil && hooks.HasHooks() {
-		log.Info("running hooks", "event", event)
-		return hooks.RunAll(event, &atmosConfig, &info, cmd, args)
-	}
-
-	return nil
-}
-
-func terraformRun(cmd *cobra.Command, actualCmd *cobra.Command, args []string) error {
-	info := getConfigAndStacksInfo("terraform", cmd, args)
-
-	if info.NeedHelp {
-		err := actualCmd.Usage()
-		if err != nil {
-			log.Fatal(err)
-		}
-		return nil
-	}
-
-	flags := cmd.Flags()
-
-	processTemplates, err := flags.GetBool("process-templates")
-	if err != nil {
-		u.PrintErrorMarkdownAndExit("", err, "")
-	}
-
-	processYamlFunctions, err := flags.GetBool("process-functions")
-	if err != nil {
-		u.PrintErrorMarkdownAndExit("", err, "")
-	}
-
-	skip, err := flags.GetStringSlice("skip")
-	if err != nil {
-		u.PrintErrorMarkdownAndExit("", err, "")
-	}
-
-	components, err := flags.GetStringSlice("components")
-	if err != nil {
-		u.PrintErrorMarkdownAndExit("", err, "")
-	}
-
-	dryRun, err := flags.GetBool("dry-run")
-	if err != nil {
-		u.PrintErrorMarkdownAndExit("", err, "")
-	}
-
-	info.ProcessTemplates = processTemplates
-	info.ProcessFunctions = processYamlFunctions
-	info.Skip = skip
-	info.Components = components
-	info.DryRun = dryRun
-
-	if info.Affected &&
-		(info.All || len(info.Components) > 0 || info.Query != "" || info.ComponentFromArg != "") {
-		u.PrintErrorMarkdownAndExit("", ErrInvalidTerraformFlags, "")
-	}
-
-	// Execute `atmos terraform <sub-command> --affected` or `atmos terraform <sub-command> --affected --stack <stack>`
-	if info.Affected {
-		// Add these flags because `atmos describe affected` needs them, but `atmos terraform --affected` does not define them
-		cmd.PersistentFlags().String("file", "", "")
-		cmd.PersistentFlags().String("format", "yaml", "")
-		cmd.PersistentFlags().Bool("verbose", false, "")
-		cmd.PersistentFlags().Bool("include-spacelift-admin-stacks", false, "")
-		cmd.PersistentFlags().Bool("include-settings", false, "")
-		cmd.PersistentFlags().Bool("upload", false, "")
-
-		a, err := e.ParseDescribeAffectedCliArgs(cmd, args)
-		if err != nil {
-			return err
-		}
-
-		a.IncludeSpaceliftAdminStacks = false
-		a.IncludeSettings = false
-		a.Upload = false
-		a.OutputFile = ""
-
-		err = e.ExecuteTerraformAffected(&a, &info)
-		if err != nil {
-			u.PrintErrorMarkdownAndExit("", err, "")
-		}
-		return nil
-	}
-
-	// Execute `atmos terraform <sub-command>` with the filters if any of the following flags are specified:
-	// `--all`
-	// `--components c1,c2`
-	// `--query <yq-expression>`
-	// `--stack` (and the `component` argument is not passed)
-	if info.All || len(info.Components) > 0 || info.Query != "" || (info.Stack != "" && info.ComponentFromArg == "") {
-		err = e.ExecuteTerraformQuery(&info)
-		if err != nil {
-			u.PrintErrorMarkdownAndExit("", err, "")
-		}
-		return nil
-	}
-
-	// Execute `atmos terraform <sub-command> <component> --stack <stack>`
-	err = e.ExecuteTerraform(info)
-	// For plan-diff, ExecuteTerraform will call OsExit directly if there are differences
-	// So if we get here, it means there were no differences or there was an error
-	if err != nil {
-		if errors.Is(err, terrerrors.ErrPlanHasDiff) {
-			// Print the error message but return the error to be handled by main.go
-			u.PrintErrorMarkdown("", err, "")
-			return err
-		}
-		// For other errors, continue with existing behavior
-		u.PrintErrorMarkdownAndExit("", err, "")
-	}
-	return nil
 }
