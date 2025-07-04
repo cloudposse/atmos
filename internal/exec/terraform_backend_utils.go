6cf9451700cbef4f92c52d14cecb338a24a51486
aknysh <andriy.knysh@gmail.com>
2025-07-03 20:44:31 -0400

    updates

--- internal/exec/terraform_backend_utils.go
+++ internal/exec/terraform_backend_utils.go
@@ -47,9 +47,18 @@ func GetComponentBackendType(sections map[string]any) string {
 	return ""
 }
 
-// GetBackendAttribute returns an attribute from the backend config.
-func GetBackendAttribute(backend map[string]any, section string) string {
-	if i, ok := backend[section].(string); ok {
+// GetBackendAssumeRole returns the `assume_role` section from the backend config.
+// https://developer.hashicorp.com/terraform/language/backend/s3#assume-role-configuration
+func GetBackendAssumeRole(backend map[string]any) map[string]any {
+	if i, ok := backend["assume_role"].(map[string]any); ok {
+		return i
+	}
+	return nil
+}
+
+// GetSectionAttribute returns an attribute from a section in the config.
+func GetSectionAttribute(section map[string]any, attribute string) string {
+	if i, ok := section[attribute].(string); ok {
 		return i
 	}
 	return ""
@@ -75,18 +84,30 @@ func GetTerraformBackendInfo(sections map[string]any) TerraformBackendInfo {
 	info.Workspace = GetComponentTerraformWorkspace(sections)
 	info.Backend = GetComponentBackend(sections)
 	info.Type = GetComponentBackendType(sections)
+
+	// If the backend is not configured in stack manifests, default to "local"
 	if info.Type == "" {
 		info.Type = cfg.BackendTypeLocal
 	}
 
+	// Process S3 backend
 	if info.Type == cfg.BackendTypeS3 {
 		info.S3 = TerraformS3BackendInfo{
-			Bucket:             GetBackendAttribute(info.Backend, "bucket"),
-			Region:             GetBackendAttribute(info.Backend, "region"),
-			Key:                GetBackendAttribute(info.Backend, "key"),
-			RoleArn:            GetBackendAttribute(info.Backend, "role_arn"),
-			WorkspaceKeyPrefix: GetBackendAttribute(info.Backend, "workspace_key_prefix"),
+			Bucket:             GetSectionAttribute(info.Backend, "bucket"),
+			Region:             GetSectionAttribute(info.Backend, "region"),
+			Key:                GetSectionAttribute(info.Backend, "key"),
+			WorkspaceKeyPrefix: GetSectionAttribute(info.Backend, "workspace_key_prefix"),
 		}
+		// Support `assume_role.role_arn` and the deprecated `role_arn` in the backend
+		var roleArn string
+		assumeRoleSection := GetBackendAssumeRole(info.Backend)
+		if assumeRoleSection != nil {
+			roleArn = GetSectionAttribute(assumeRoleSection, "role_arn")
+		}
+		if roleArn == "" {
+			roleArn = GetSectionAttribute(info.Backend, "role_arn")
+		}
+		info.S3.RoleArn = roleArn
 	}
 
 	return info
