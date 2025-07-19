bdd7efcb91127d0bd4f094cd360dff63a88a98a0
aknysh <andriy.knysh@gmail.com>
2025-07-19 00:54:41 -0400

    updates

---
+++ cmd/packer_inspect.go
@@ -0,0 +1,34 @@
+// https://developer.hashicorp.com/packer/docs/commands/inspect
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// Command: `atmos packer inspect`.
+var (
+	packerInspectShort = "Inspect a Packer configuration."
+	packerInspectLong  = `The command takes a Packer template and outputs the various components the template defines.
+
+Example usage:
+  atmos packer inspect <component> --stack <stack>
+  atmos packer inspect <component> -s <stack>
+`
+)
+
+// packerInspectCmd represents the `atmos packer inspect` command.
+var packerInspectCmd = &cobra.Command{
+	Use:                "inspect",
+	Aliases:            []string{},
+	Short:              packerInspectShort,
+	Long:               packerInspectLong,
+	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return packerRun(cmd, "inspect", args)
+	},
+}
+
+func init() {
+	packerCmd.AddCommand(packerInspectCmd)
+}
---
+++ cmd/packer_validate.go
@@ -0,0 +1,36 @@
+// https://developer.hashicorp.com/packer/docs/commands/validate
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// Command: `atmos packer validate`.
+var (
+	packerValidateShort = "Validate a Packer template."
+	packerValidateLong  = `The command is used to validate the syntax and configuration of a Packer template.
+
+Example usage:
+  atmos packer validate <component> --stack <stack> [options]
+  atmos packer validate <component> -s <stack> [options]
+
+To see all available options, refer to https://developer.hashicorp.com/packer/docs/commands/validate
+`
+)
+
+// packerValidateCmd represents the `atmos packer validate` command.
+var packerValidateCmd = &cobra.Command{
+	Use:                "validate",
+	Aliases:            []string{},
+	Short:              packerValidateShort,
+	Long:               packerValidateLong,
+	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return packerRun(cmd, "validate", args)
+	},
+}
+
+func init() {
+	packerCmd.AddCommand(packerValidateCmd)
+}
