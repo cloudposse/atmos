fa99a37a77384afcb154aff76a72a33823ae94da
aknysh <andriy.knysh@gmail.com>
2025-07-21 19:55:36 -0400

    updates

--- internal/exec/packer.go
+++ internal/exec/packer.go
@@ -66,7 +66,7 @@ func ExecutePacker(info schema.ConfigAndStacksInfo) error {
 	}
 
 	// Check if the component is allowed to be provisioned (`metadata.type` attribute)
-	if (info.SubCommand == "sync" || info.SubCommand == "apply" || info.SubCommand == "deploy") && info.ComponentIsAbstract {
+	if (info.SubCommand == "build") && info.ComponentIsAbstract {
 		return fmt.Errorf("abstract component '%s' cannot be provisioned since it's explicitly prohibited from being deployed "+
 			"by 'metadata.type: abstract' attribute", filepath.Join(info.ComponentFolderPrefix, info.Component))
 	}
@@ -75,7 +75,7 @@ func ExecutePacker(info schema.ConfigAndStacksInfo) error {
 	if info.ComponentIsLocked {
 		// Allow read-only commands, block modification commands
 		switch info.SubCommand {
-		case "sync", "apply", "deploy", "delete", "destroy":
+		case "build":
 			return fmt.Errorf("component `%s` is locked and cannot be modified (metadata.locked = true)",
 				filepath.Join(info.ComponentFolderPrefix, info.Component))
 		}
@@ -97,64 +97,15 @@ func ExecutePacker(info schema.ConfigAndStacksInfo) error {
 		}
 	}
 
-	// Handle `helmfile deploy` custom command
-	if info.SubCommand == "deploy" {
-		info.SubCommand = "sync"
-	}
-
-	context := cfg.GetContextFromVars(info.ComponentVarsSection)
-
-	envVarsEKS := []string{}
-
-	if atmosConfig.Components.Helmfile.UseEKS {
-		// Prepare AWS profile
-		helmAwsProfile := cfg.ReplaceContextTokens(context, atmosConfig.Components.Helmfile.HelmAwsProfilePattern)
-		log.Debug("Using AWS_PROFILE", "profile", helmAwsProfile)
-
-		// Download kubeconfig by running `aws eks update-kubeconfig`
-		kubeconfigPath := fmt.Sprintf("%s/%s-kubecfg", atmosConfig.Components.Helmfile.KubeconfigPath, info.ContextPrefix)
-		clusterName := cfg.ReplaceContextTokens(context, atmosConfig.Components.Helmfile.ClusterNamePattern)
-		log.Debug("Downloading and saving kubeconfig", "cluster", clusterName, "path", kubeconfigPath)
-
-		err = ExecuteShellCommand(
-			atmosConfig,
-			"aws",
-			[]string{
-				"--profile",
-				helmAwsProfile,
-				"eks",
-				"update-kubeconfig",
-				fmt.Sprintf("--name=%s", clusterName),
-				fmt.Sprintf("--region=%s", context.Region),
-				fmt.Sprintf("--kubeconfig=%s", kubeconfigPath),
-			},
-			componentPath,
-			nil,
-			info.DryRun,
-			info.RedirectStdErr,
-		)
-		if err != nil {
-			return err
-		}
-
-		envVarsEKS = append(envVarsEKS, []string{
-			fmt.Sprintf("AWS_PROFILE=%s", helmAwsProfile),
-			fmt.Sprintf("KUBECONFIG=%s", kubeconfigPath),
-		}...)
-	}
-
 	// Print command info
 	log.Debug("\nCommand info:")
 	log.Debug("Helmfile binary: " + info.Command)
 	log.Debug("Helmfile command: " + info.SubCommand)
-
-	log.Debug("Global", "options", info.GlobalOptions)
-
 	log.Debug("Arguments and flags", "additional", info.AdditionalArgsAndFlags)
 	log.Debug("Component: " + info.ComponentFromArg)
 
 	if len(info.BaseComponent) > 0 {
-		log.Debug("Helmfile component: " + info.BaseComponent)
+		log.Debug("Packer component: " + info.BaseComponent)
 	}
 
 	if info.Stack == info.StackFromArg {
@@ -180,35 +131,6 @@ func ExecutePacker(info schema.ConfigAndStacksInfo) error {
 		fmt.Sprintf("STACK=%s", info.Stack),
 	}...)
 
-	// Append the context ENV vars (first check if they are not set by the caller)
-	env := os.Getenv("NAMESPACE")
-	if env == "" {
-		envVars = append(envVars, fmt.Sprintf("NAMESPACE=%s", context.Namespace))
-	}
-	env = os.Getenv("TENANT")
-	if env == "" {
-		envVars = append(envVars, fmt.Sprintf("TENANT=%s", context.Tenant))
-	}
-	env = os.Getenv("ENVIRONMENT")
-	if env == "" {
-		envVars = append(envVars, fmt.Sprintf("ENVIRONMENT=%s", context.Environment))
-	}
-	env = os.Getenv("STAGE")
-	if env == "" {
-		envVars = append(envVars, fmt.Sprintf("STAGE=%s", context.Stage))
-	}
-	env = os.Getenv("REGION")
-	if env == "" {
-		envVars = append(envVars, fmt.Sprintf("REGION=%s", context.Region))
-	}
-
-	if atmosConfig.Components.Helmfile.KubeconfigPath != "" {
-		envVars = append(envVars, fmt.Sprintf("KUBECONFIG=%s", atmosConfig.Components.Helmfile.KubeconfigPath))
-	}
-
-	if atmosConfig.Components.Helmfile.UseEKS {
-		envVars = append(envVars, envVarsEKS...)
-	}
 	envVars = append(envVars, fmt.Sprintf("ATMOS_CLI_CONFIG_PATH=%s", atmosConfig.CliConfigPath))
 	basePath, err := filepath.Abs(atmosConfig.BasePath)
 	if err != nil {
