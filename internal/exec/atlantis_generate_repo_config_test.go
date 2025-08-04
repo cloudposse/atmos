179bc5973d19932220b1b219c7812804846cdfee
Igor Rodionov <goruha@gmail.com>
2025-08-04 18:15:43 +0200

    Run short tests

--- internal/exec/atlantis_generate_repo_config_test.go
+++ internal/exec/atlantis_generate_repo_config_test.go
@@ -38,7 +38,6 @@ func TestExecuteAtlantisGenerateRepoConfigWithStackNameTemplate(t *testing.T) {
 }
 
 func TestExecuteAtlantisGenerateRepoConfigAffectedOnly(t *testing.T) {
-	// t.Skip("Skipping test in short mode")
 	stacksPath := "../../tests/fixtures/scenarios/atlantis-generate-repo-config"
 	t.Setenv("ATMOS_CLI_CONFIG_PATH", stacksPath)
 	t.Setenv("ATMOS_BASE_PATH", stacksPath)
