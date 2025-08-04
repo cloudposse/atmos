fb8fcec5ae21de726632a7ff79b8f1863bcbff99
Igor Rodionov <goruha@gmail.com>
2025-08-04 20:51:27 +0200

    Run short tests

--- internal/terraform_backend/terraform_backend_s3_test.go
+++ internal/terraform_backend/terraform_backend_s3_test.go
@@ -19,7 +19,6 @@ import (
 
 func TestReadTerraformBackendS3_InvalidConfig(t *testing.T) {
 	// t.Skip("Skipping test in short mode")
-
 	tests := []struct {
 		name          string
 		componentData map[string]any
