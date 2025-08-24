6f9c37385f9a2962c73461b734880d19df41e72b
Sam Tholiya <tholiya.sam@outlook.in>
2025-08-24 14:20:35 +0200

    updated documentation

--- internal/exec/terraform_clean.go
+++ internal/exec/terraform_clean.go
@@ -523,7 +523,7 @@ func handleCleanSubCommand(info schema.ConfigAndStacksInfo, componentPath string
 		}
 		err := DeletePaths(userFiles)
 		if err != nil {
-			return fmt.Errorf("deleting user-specified clean paths: %w", err)
+			return err
 		}
 		deleteFolders(folders, relativePath, atmosConfig)
 		if len(tfDataDirFolders) > 0 {
