b522af0a50f4e2e9d190341ddec1881cdc389c96
RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
2025-09-05 11:17:21 -0400

    feat: update atmos' pronouns
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- cmd/ansible_vault.go
+++ cmd/ansible_vault.go
@@ -16,7 +16,7 @@ var ansibleVaultCmd = &cobra.Command{
 	Use:                "vault",
 	Aliases:            []string{},
 	Short:              "Encrypt and decrypt files within Ansible infrastructure.",
-	Long:               "his command allows you to create, edit, encrypt, decrypt, and view encrypted files using Ansible Vault.",
+	Long:               "This command allows you to create, edit, encrypt, decrypt, and view encrypted files using Ansible Vault.",
 	Example:            ansibleVaultUsageMarkdown,
 	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: true},
 	RunE: func(cmd *cobra.Command, args []string) error {
