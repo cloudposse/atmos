172a04ecee5b51cb3aad852761757241e781ac05
Erik Osterman <erik@cloudposse.com>
2026-06-19 08:32:19 -0500

    Use test kit for docs tests

--- cmd/docs_test.go
+++ cmd/docs_test.go
@@ -11,6 +11,8 @@ import (
 // TestDocsCmd_WithoutStacks verifies that docs command does not require stack configuration.
 // This test documents that the docs command uses InitCliConfig with processStacks=false.
 func TestDocsCmd_WithoutStacks(t *testing.T) {
+	_ = NewTestKit(t)
+
 	// This test documents that docs command does not process stacks
 	// by verifying InitCliConfig is called with processStacks=false in docs.go:38
 	// No runtime test needed - this is enforced by code structure.
@@ -18,11 +20,9 @@ func TestDocsCmd_WithoutStacks(t *testing.T) {
 }
 
 func TestDocsCmdOpensDefaultDocsURL(t *testing.T) {
+	_ = NewTestKit(t)
+
 	var openedURL string
-	originalOpenDocsURL := openDocsURL
-	t.Cleanup(func() {
-		openDocsURL = originalOpenDocsURL
-	})
 	openDocsURL = func(url string) error {
 		openedURL = url
 		return nil
@@ -33,11 +33,9 @@ func TestDocsCmdOpensDefaultDocsURL(t *testing.T) {
 }
 
 func TestDocsCmdReturnsDefaultDocsOpenError(t *testing.T) {
+	_ = NewTestKit(t)
+
 	openErr := errors.New("browser unavailable")
-	originalOpenDocsURL := openDocsURL
-	t.Cleanup(func() {
-		openDocsURL = originalOpenDocsURL
-	})
 	openDocsURL = func(string) error {
 		return openErr
 	}
