49ba6fc8ff26908e2e3228aeeba34d82b72a1be4
Mikhail Shirkov <shirkevich@gmail.com>
2026-08-06 01:13:52 +0400

    fix: preserve Helm timeout error context

--- pkg/component/helm/client.go
+++ pkg/component/helm/client.go
@@ -192,7 +192,7 @@ func upgradeRelease(ctx context.Context, actx *actionContext, spec *chartSpec, d
 	rel, err := client.RunWithContext(ctx, spec.ReleaseName, loaded, spec.Values)
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
-			return "", ctxErr
+			return "", releaseOperationError("upgrade", spec, errors.Join(ctxErr, err))
 		}
 		return "", releaseOperationError("upgrade", spec, err)
 	}
